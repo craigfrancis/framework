95af39c38bbd1b7bbda7f97ac2b85236cc4ee4e8
Craig Francis <craig@craigfrancis.co.uk>
2010-04-20 16:07:09 +0000

    Add database query examples

--- framework/0.1/system/database.php
+++ framework/0.1/system/database.php
@@ -1,5 +1,24 @@
 <?php
 
-
+//	db('SELECT
+//			*
+//		FROM
+//			' . DB_T_PREFIX . 'table_name
+//		WHERE
+//			field = val';
+//
+//	db::ex('SELECT
+//				*
+//			FROM
+//				' . DB_T_PREFIX . 'table_name
+//			WHERE
+//				field = val';
+//
+//	$db->query('SELECT
+//					*
+//				FROM
+//					' . DB_T_PREFIX . 'table_name
+//				WHERE
+//					field = val');
 
 ?>
