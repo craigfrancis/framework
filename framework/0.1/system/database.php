a7b1a07b3d2a6534d5f8d3b7050efcfab9c1ca22
Craig Francis <craig@craigfrancis.co.uk>
2010-04-29 08:28:32 +0000

    Update database notes

--- framework/0.1/system/database.php
+++ framework/0.1/system/database.php
@@ -1,5 +1,35 @@
 <?php
 
+//--------------------------------------------------
+//
+// $rst = db:q('SELECT
+// 			FROM');
+//
+// $rst = $myDb:q('SELECT
+// 				FROM');
+//
+// $rst = $this->db:q('SELECT
+// 					FROM');
+//
+// In this example, we could return a "db_result" object,
+// which could be used in the form of:
+//
+// if ($rst->num_rows() > 0) {
+// }
+//
+// while ($rst->next()) {
+// 	$user[]['address'] = $rst->fetch_assoc();
+// 	$user[]['address'] = $rst->get('address');
+// 	$user[]['address'] = $rst->getAddress();
+// 	$user[]['address'] = $rst->address;
+// }
+//
+// This may be helpful when searching for all
+// references to address... but remember tab
+// completion.
+//
+//--------------------------------------------------
+//
 //	db('SELECT
 //			*
 //		FROM
@@ -20,12 +50,16 @@
 //					' . DB_T_PREFIX . 'table_name
 //				WHERE
 //					field = val');
-
+//
+//--------------------------------------------------
+//
 //	db::ex('SELECT
 //			FROM');
 //	$db->query('SELECT
 //				FROM');
 //	$this->query('SELECT
 //				FROM');
+//
+//--------------------------------------------------
 
 ?>
