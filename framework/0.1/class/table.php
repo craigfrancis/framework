8d1379d65f70330c27fedf0c144d03a32093d1ec
Craig Francis <craig@craigfrancis.co.uk>
2011-07-07 09:53:25 +0000

    Initial addition of table object

--- framework/0.1/class/table.php
+++ framework/0.1/class/table.php
@@ -1,5 +1,665 @@
 <?php
 
-// TODO: Get from /a/php/table.php
+	class table {
+
+		function table() {
+
+			//--------------------------------------------------
+			// Defaults
+
+				$this->headings = array();
+				$this->heading_row = 0;
+				$this->footers = array();
+				$this->footer_row = 0;
+				$this->rows = array();
+
+				$this->class_name = 'basic_table';
+				$this->url_this_page = NULL;
+				$this->no_records_html = 'No records found';
+				$this->data_inherit_heading_class = true;
+				$this->footer_inherit_heading_class = false;
+
+				$this->sort_name = NULL;
+				$this->sort_field = NULL;
+				$this->sort_default_field = NULL;
+				$this->sort_default_order = NULL;
+				$this->sort_fields = array();
+				$this->sort_order = NULL;
+				$this->sort_active_asc_prefix_html = '';
+				$this->sort_active_asc_suffix_html = '';
+				$this->sort_active_desc_prefix_html = '';
+				$this->sort_active_desc_suffix_html = '';
+				$this->sort_inactive_prefix_html = '';
+				$this->sort_inactive_suffix_html = '';
+
+		}
+
+		function this_page_url_set($url) {
+			$this->url_this_page = $url;
+		}
+
+		function class_set($class_name) {
+			$this->class_name = $class_name;
+		}
+
+		function sort_name_set($name) {
+			$this->sort_name = $name;
+			$this->sort_field = data($this->sort_name . '_name');
+			$this->sort_order = data($this->sort_name . '_order');
+		}
+
+		function default_sort_set($field, $order = 'asc') {
+			$this->sort_default_field = $field;
+			$this->sort_default_order = $order;
+		}
+
+		function sort_field_get() {
+			if (in_array($this->sort_field, $this->sort_fields)) {
+				return $this->sort_field;
+			} else {
+				return $this->sort_default_field; // An unrecognised value supplied by GPC
+			}
+		}
+
+		function sort_order_get() {
+
+			if ($this->sort_order == 'desc' || $this->sort_order == 'asc') {
+				return $this->sort_order; // Stop bad values from GPC
+			}
+
+			if ($this->sort_default_order == 'desc' || $this->sort_default_order == 'asc') {
+				return $this->sort_default_order; // Mistakes in program
+			}
+
+			return 'asc';
+
+		}
+
+		function sort_sql_get() {
+
+			$sql_order_by = $this->sort_field_get();
+
+			if (preg_match('/^([^,]+)(,.*)$/', $sql_order_by, $matches)) {
+				return $matches[1] . ' ' . $this->sort_order_get() . $matches[2];
+			} else {
+				return $sql_order_by . ' ' . $this->sort_order_get();
+			}
+
+		}
+
+		function sort_url_get($field, $order) {
+			$url = new url($this->url_this_page);
+			$url->param($this->sort_name . '_name', $field);
+			$url->param($this->sort_name . '_order', $order);
+			return $url->get();
+		}
+
+		function active_asc_prefix_set($content) {
+			$this->sort_active_asc_prefix_html = html($content);
+		}
+
+		function active_asc_prefix_set_html($content_html) {
+			$this->sort_active_asc_prefix_html = $content_html;
+		}
+
+		function active_asc_suffix_set($content) {
+			$this->sort_active_asc_suffix_html = html($content);
+		}
+
+		function active_asc_suffix_set_html($content_html) {
+			$this->sort_active_asc_suffix_html = $content_html;
+		}
+
+		function active_desc_prefix_set($content) {
+			$this->sort_active_desc_prefix_html = html($content);
+		}
+
+		function active_desc_prefix_set_html($content_html) {
+			$this->sort_active_desc_prefix_html = $content_html;
+		}
+
+		function active_desc_suffix_set($content) {
+			$this->sort_active_desc_suffix_html = html($content);
+		}
+
+		function active_desc_suffix_set_html($content_html) {
+			$this->sort_active_desc_suffix_html = $content_html;
+		}
+
+		function inactive_prefix_set($content) {
+			$this->sort_inactive_prefix_html = html($content);
+		}
+
+		function inactive_prefix_set_html($content_html) {
+			$this->sort_inactive_prefix_html = $content_html;
+		}
+
+		function inactive_suffix_set($content) {
+			$this->sort_inactive_suffix_html = html($content);
+		}
+
+		function inactive_suffix_set_html($content_html) {
+			$this->sort_inactive_suffix_html = $content_html;
+		}
+
+		function heading_add($heading, $sort_name = NULL, $class_name = '', $colspan = 1) {
+			$this->heading_add_html(html($heading), $sort_name, $class_name, $colspan);
+		}
+
+		function heading_add_html($heading_html, $sort_name = NULL, $class_name = '', $colspan = 1) {
+
+			if (!isset($this->headings[$this->heading_row])) {
+				$this->headings[$this->heading_row] = array();
+			}
+
+			$this->headings[$this->heading_row][] = array(
+					'html' => $heading_html,
+					'sort_name' => $sort_name,
+					'class_name' => $class_name,
+					'colspan' => $colspan,
+				);
+
+			if ($sort_name !== NULL && $sort_name !== '') {
+				$this->sort_fields[] = $sort_name;
+			}
+
+		}
+
+		function end_heading_row() {
+			$this->heading_row++;
+		}
+
+		function footer_add($footer, $class_name = '', $colspan = 1) {
+			$this->footer_add_html(html($footer), $class_name, $colspan);
+		}
+
+		function footer_add_html($footer_html, $class_name = '', $colspan = 1) {
+
+			if (!isset($this->footers[$this->footer_row])) {
+				$this->footers[$this->footer_row] = array();
+			}
+
+			$this->footers[$this->footer_row][] = array(
+					'html' => $footer_html,
+					'class_name' => $class_name,
+					'colspan' => $colspan,
+				);
+
+		}
+
+		function end_footer_row() {
+
+			if (!isset($this->footers[$this->footer_row])) {
+				$this->footers[$this->footer_row] = array();
+			}
+
+			$this->footer_row++;
+
+		}
+
+		function row_add($row = NULL, $class_name = '') {
+			$this->rows[] = array(
+					'row' => $row,
+					'class_name' => $class_name
+				);
+		}
+
+		function no_records_message_set($no_records) {
+			$this->no_records_html = html($no_records);
+		}
+
+		function no_records_message_set_html($no_records_html) {
+			$this->no_records_html = $no_records_html;
+		}
+
+		function html() {
+
+			//--------------------------------------------------
+			// Current sort - inc support for defaults
+
+				$current_sort = $this->sort_field_get();
+				$sort_asc = ($this->sort_order_get() == 'asc');
+
+			//--------------------------------------------------
+			// Headings
+
+				$col_sorted = array();
+				$col_class = array();
+				$col_count = 0;
+
+				$output_html = '
+					<table cellspacing="0" cellpadding="1" border="1" class="' . html($this->class_name) . '">
+						<thead>';
+
+				foreach ($this->headings as $c_heading_row) {
+
+					$col_id = 0;
+
+					$output_html .= '
+							<tr>';
+
+					foreach ($c_heading_row as $c_heading) {
+
+						if (!isset($col_sorted[$col_id])) {
+							$col_sorted[$col_id] = false;
+							$col_class[$col_id] = '';
+						}
+
+						if ($this->data_inherit_heading_class && $c_heading['class_name'] != '') {
+							$col_class[$col_id] .= ' ' . $c_heading['class_name'];
+						}
+
+						if ($c_heading['html'] == '') {
+							$c_heading['html'] = '&#xA0;';
+						}
+
+						$attributes_html = ' scope="col" class="' . html($c_heading['class_name']) . '"';
+						if ($c_heading['colspan'] > 1) {
+							$attributes_html .= ' colspan="' . html($c_heading['colspan']) . '"';
+						}
+
+						if ($this->sort_name === NULL || $c_heading['sort_name'] === NULL) {
+
+							$output_html .= '
+								<th' . $attributes_html . '>' . $c_heading['html'] . '</th>';
+
+						} else if ($current_sort == $c_heading['sort_name']) {
+
+							$url = $this->sort_url_get($c_heading['sort_name'], ($sort_asc ? 'desc' : 'asc'));
+
+							$output_html .= '
+								<th' . $attributes_html . '><a href="' . html($url) . '">' . ($sort_asc ? $this->sort_active_asc_prefix_html : $this->sort_active_desc_prefix_html) . $c_heading['html'] . ($sort_asc ? $this->sort_active_asc_suffix_html : $this->sort_active_desc_suffix_html) . '</a></th>';
+
+							$col_sorted[$col_id] = true;
+
+						} else {
+
+							$url = $this->sort_url_get($c_heading['sort_name'], 'asc');
+
+							$output_html .= '
+								<th' . $attributes_html . '><a href="' . html($url) . '">' . $this->sort_inactive_prefix_html . $c_heading['html'] . $this->sort_inactive_suffix_html . '</a></th>';
+
+						}
+
+						$col_id += $c_heading['colspan'];
+
+					}
+
+					if ($col_id > $col_count) {
+						$col_count = $col_id;
+					}
+
+					$output_html .= '
+							</tr>';
+
+				}
+
+				$output_html .= '
+						</thead>';
+
+			//--------------------------------------------------
+			// Footer
+
+				if (count($this->footers)) {
+
+					$output_html .= '
+						<tfoot>';
+
+					foreach ($this->footers as $c_footer_row) {
+
+						$col_id = 0;
+
+						$output_html .= '
+							<tr>';
+
+						foreach ($c_footer_row as $c_footer) {
+
+							if ($c_footer['html'] == '') {
+								$c_footer['html'] = '&#xA0;';
+							}
+
+							$class = $c_footer['class_name'];
+
+							if ($this->footer_inherit_heading_class && isset($col_class[$col_id]) && $col_class[$col_id] != '') {
+								$class .= ' ' . $col_class[$col_id];
+							}
+
+							if ($this->footer_inherit_heading_class && isset($col_sorted[$col_id]) && $col_sorted[$col_id]) {
+								$class .= ' sorted';
+							}
+
+							$attributes_html = ' class="' . html(trim($class)) . '"';
+							if ($c_footer['colspan'] > 1) {
+								$attributes_html .= ' colspan="' . html($c_footer['colspan']) . '"';
+							}
+
+							$output_html .= '
+								<td' . $attributes_html . '>' . $c_footer['html'] . '</td>';
+
+							$col_id += $c_footer['colspan'];
+
+						}
+
+						$output_html .= '
+							</tr>';
+
+					}
+
+					$output_html .= '
+						</tfoot>';
+
+				}
+
+			//--------------------------------------------------
+			// Data
+
+				$output_html .= '
+						<tbody>';
+
+				$row_id = 0;
+
+				foreach (array_keys($this->rows) as $c_row) {
+
+					$row_class = $this->rows[$c_row]['class_name'] . ($row_id++ % 2 ? ' even' : '');
+
+					if ($this->rows[$c_row]['row'] === NULL) {
+
+						$data = array();
+						for ($k = 0; $k < $col_count; $k++) {
+							$data[] = array(
+									'html' => '',
+									'class_name' => '',
+									'colspan' => 1,
+								);
+						}
+
+						$row_class .= ' blank_row';
+
+					} else {
+
+						$data = $this->rows[$c_row]['row']->data;
+
+					}
+
+					$output_html .= '
+							<tr class="' . html(trim($row_class)) . '">';
+
+					$col_id = 0;
+
+					foreach ($data as $c_data) {
+
+						//--------------------------------------------------
+						// Cell class
+
+							$class = $c_data['class_name'];
+
+							if (isset($col_class[$col_id]) && $col_class[$col_id] != '') {
+								$class .= ' ' . $col_class[$col_id];
+							}
+
+							if (isset($col_sorted[$col_id]) && $col_sorted[$col_id]) {
+								$class .= ' sorted';
+							}
+
+						//--------------------------------------------------
+						// Attributes
+
+							$attributes_html = ' class="' . html(trim($class)) . '"';
+							if ($c_data['colspan'] > 1) {
+								$attributes_html .= ' colspan="' . html($c_data['colspan']) . '"';
+							}
+
+						//--------------------------------------------------
+						// HTML
+
+							if ($c_data['html'] == '') {
+								$c_data['html'] = '&#xA0;';
+							}
+
+							$output_html .= '
+								<td' . $attributes_html . '>' . $c_data['html'] . '</td>';
+
+						//--------------------------------------------------
+						// Column ID
+
+							$col_id += $c_data['colspan'];
+
+					}
+
+					$output_html .= '
+							</tr>';
+
+				}
+
+			//--------------------------------------------------
+			// Error message
+
+				if (count($this->rows) == 0) {
+
+					$output_html .= '
+							<tr>
+								<td colspan="' . html($col_count) . '" class="no_results">' . $this->no_records_html . '</td>
+							</tr>';
+
+				}
+
+			//--------------------------------------------------
+			// End
+
+				$output_html .= '
+						</tbody>
+					</table>';
+
+			//--------------------------------------------------
+			// Return
+
+				return $output_html;
+
+		}
+
+		function csv_data_get() {
+
+			//--------------------------------------------------
+			// Headings
+
+				$col_count = 0;
+
+				$csv_output = '';
+
+				foreach ($this->headings as $c_heading_row) {
+
+					$col_id = 0;
+
+					foreach ($c_heading_row as $c_heading) {
+
+						$csv_output .= '"' . csv(html_decode(strip_tags($c_heading['html']))) . '",';
+
+						for ($k = 1; $k < $c_heading['colspan']; $k++) {
+							$csv_output .= '"",';
+						}
+
+						$col_id += $c_heading['colspan'];
+
+					}
+
+					if ($col_id > $col_count) {
+						$col_count = $col_id;
+					}
+
+					$csv_output .= "\n";
+
+				}
+
+			//--------------------------------------------------
+			// Data
+
+				foreach (array_keys($this->rows) as $c_row) {
+
+					if ($this->rows[$c_row]['row'] === NULL) {
+
+						for ($k = 0; $k < $col_count; $k++) {
+							$csv_output .= '"",';
+						}
+
+					} else {
+
+						foreach ($this->rows[$c_row]['row']->data as $c_data) {
+
+							$csv_output .= '"' . csv(html_decode(strip_tags($c_data['html']))) . '",';
+
+							for ($k = 1; $k < $c_data['colspan']; $k++) {
+								$csv_output .= '"",';
+							}
+
+						}
+
+					}
+
+					$csv_output .= "\n";
+
+				}
+
+			//--------------------------------------------------
+			// Error message
+
+				if (count($this->rows) == 0) {
+
+					$csv_output .= '"' . csv(html_decode(strip_tags($this->no_records_html))) . '",';
+
+					for ($k = 0; $k < ($col_count - 1); $k++) {
+						$csv_output .= '"",';
+					}
+
+					$csv_output .= "\n";
+
+				}
+
+			//--------------------------------------------------
+			// Footer
+
+				if (count($this->footers)) {
+
+					foreach ($this->footers as $c_footer_row) {
+
+						foreach ($c_footer_row as $c_footer) {
+
+							$csv_output .= '"' . csv(html_decode(strip_tags($c_footer['html']))) . '",';
+
+							for ($k = 1; $k < $c_footer['colspan']; $k++) {
+								$csv_output .= '"",';
+							}
+
+						}
+
+						$csv_output .= "\n";
+
+					}
+
+				}
+
+			//--------------------------------------------------
+			// Clean end of lines
+
+				$csv_output = preg_replace('/,$/m', '', $csv_output);
+
+			//--------------------------------------------------
+			// Return
+
+				return $csv_output;
+
+		}
+
+		function csv_download($file_name, $inline = false) {
+
+			//--------------------------------------------------
+			// Data
+
+				$csv_output = $this->csv_data_get();
+
+			//--------------------------------------------------
+			// No debug output
+
+				config::set('debug.show', false);
+
+			//--------------------------------------------------
+			// Convert characterset
+
+				$new_charset = 'ISO-8859-1';
+
+				if ($new_charset != config::get('output.charset')) {
+
+					$csv_output = iconv(config::get('output.charset'), $new_charset . '//TRANSLIT', $csv_output);
+
+					config::set('output.charset', $new_charset);
+
+				}
+
+			//--------------------------------------------------
+			// Send headers
+
+				if ($inline) {
+
+					mime_set('application/csv');
+
+					header('Content-disposition: attachment; filename="'. head($file_name) . '"');
+
+				} else {
+
+					mime_set('text/plain');
+
+				}
+
+				header('Expires: ' . head(date('D, d M Y 00:00:00')) . ' GMT');
+				header('Accept-Ranges: bytes');
+				header('Content-Length: ' . head(strlen($csv_output)));
+
+			//--------------------------------------------------
+			// IE does not like 'attachment' files on HTTPS
+
+				header('Cache-control:');
+				header('Expires:');
+				header('Pragma:');
+
+			//--------------------------------------------------
+			// End by sending the data to the browser
+
+				exit($csv_output);
+
+		}
+
+		public function __toString() { // (PHP 5.2)
+			return $this->html();
+		}
+
+	}
+
+	class table_row {
+
+		function table_row() {
+
+			//--------------------------------------------------
+			// Defaults
+
+				$this->data = array();
+
+		}
+
+		function cell_add($content, $class_name = '', $colspan = 1) {
+			$this->data[] = array(
+					'html' => html($content),
+					'class_name' => $class_name,
+					'colspan' => $colspan,
+				);
+		}
+
+		function cell_add_html($content_html, $class_name = '', $colspan = 1) {
+			$this->data[] = array(
+					'html' => $content_html,
+					'class_name' => $class_name,
+					'colspan' => $colspan,
+				);
+		}
+
+	}
 
 ?>
