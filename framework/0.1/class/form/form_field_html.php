d1dd5d62d8c432cee267c38625eecbc0a4460d99
Craig Francis <craig@craigfrancis.co.uk>
2011-09-23 15:26:13 +0000

    Add custom HTML field type - full control (over info field)

---
+++ framework/0.1/class/form/form_field_html.php
@@ -0,0 +1,52 @@
+<?php
+
+	class form_field_html_base extends form_field {
+
+		//--------------------------------------------------
+		// Variables
+
+			protected $value_html;
+
+		//--------------------------------------------------
+		// Setup
+
+			public function __construct($form, $label, $name = NULL) {
+				$this->_setup_html($form, $label, $name);
+			}
+
+			protected function _setup_html($form, $label, $name = NULL) {
+
+				//--------------------------------------------------
+				// Perform the standard field setup
+
+					$this->_setup($form, $label, $name);
+
+				//--------------------------------------------------
+				// Value
+
+					$this->value_html = '';
+
+				//--------------------------------------------------
+				// Additional field configuration
+
+					$this->type = 'html';
+
+			}
+
+		//--------------------------------------------------
+		// Value
+
+			public function value_set_html($html) {
+				$this->value_html = $html;
+			}
+
+		//--------------------------------------------------
+		// HTML
+
+			public function html() {
+				return $this->value_html;
+			}
+
+	}
+
+?>
