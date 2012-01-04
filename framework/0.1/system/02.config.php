908edc0a3cecac3775a62cb85c5ee0b6a90eda08
Craig Francis <craig@craigfrancis.co.uk>
2012-01-04 15:08:45 +0000

    Set email prefix to Stage/Demo, or blank on Live

--- framework/0.1/system/02.config.php
+++ framework/0.1/system/02.config.php
@@ -247,7 +247,7 @@
 
 		config::set_default('email.from_name', config::get('output.site_name'));
 		config::set_default('email.from_email', 'noreply@example.com');
-		config::set_default('email.prefix', (SERVER == 'live' ? '' : ucfirst(SERVER)));
+		config::set_default('email.subject_prefix', (SERVER == 'live' ? '' : ucfirst(SERVER)));
 		config::set_default('email.error', NULL);
 
 	//--------------------------------------------------
