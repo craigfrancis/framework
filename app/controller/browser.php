10454b65b3db20cd75eb382d081703768dac11c8
Craig Francis <craig@craigfrancis.co.uk>
2012-05-06 14:51:40 +0000

    Start parsing the DOM in socket_browser

--- app/controller/browser.php
+++ app/controller/browser.php
@@ -4,7 +4,35 @@
 
 		public function action_index() {
 
-			$browser = new socket_browser();
+			//--------------------------------------------------
+			// Config
+
+				define('WORLD_PAY_REF', 3);
+				define('WORLD_PAY_TEST_MODE', 0); // Use 100 to test, and use visa card "4911830000000"
+				define('WORLD_PAY_INST_ID', 264397);
+				define('WORLD_PAY_VAT', 20);
+				define('WORLD_PAY_SIGNATURE', 'dfhshsvhausq');
+				define('WORLD_PAY_MERCHANT_CODE', 'GILLMANSOAMEM1');
+				define('WORLD_PAY_ADMIN_USER', 'system@gillmansoame');
+				define('WORLD_PAY_ADMIN_PASS', 'asjhbd8214');
+
+			//--------------------------------------------------
+			// Browser object
+
+				$browser = new socket_browser();
+
+			//--------------------------------------------------
+			// Login page
+
+				if (WORLD_PAY_TEST_MODE > 0) {
+					$next_url = 'https://secure.worldpay.com/sso/public/auth/login.html?serviceIdentifier=applicationlisttest';
+				} else {
+					$next_url = 'https://secure.worldpay.com/sso/public/auth/login.html?serviceIdentifier=merchantadmin';
+				}
+
+				$browser->get($next_url);
+
+				$browser->form_select();
 
 		}
 
--- framework/0.1/class/socket/socket_browser.php
+++ framework/0.1/class/socket/socket_browser.php
@@ -1,11 +1,22 @@
 <?php
 
+/***************************************************
+
+	//--------------------------------------------------
+	// Example setup
+
+
+
+***************************************************/
+
 	class socket_browser_base extends check {
 
 		//--------------------------------------------------
 		// Variables
 
 			private $socket;
+			private $current_url;
+			private $host_cookies;
 
 		//--------------------------------------------------
 		// Setup
@@ -15,15 +26,123 @@
 			}
 
 			protected function _setup() {
+
 				$this->socket = new socket();
 				$this->socket->exit_on_error_set(false);
+
+				$this->current_url = NULL;
+				$this->host_cookies = array();
+
 			}
 
 		//--------------------------------------------------
-		//
+		// Request
 
-			protected function url_load() {
+			public function get($url) {
+
+				//--------------------------------------------------
+				// Get page
+
+config::set('debug.show', false);
+$this->current_url = $url;
+return;
+
+					do {
+
+						$this->socket->get($url);
+
+						$this->current_url = $url;
+
+					} while (($url = $this->socket->response_header_get('Location')) !== NULL);
+
 			}
+
+		//--------------------------------------------------
+		// Current page
+
+			public function current_url_get() {
+				return $this->current_url;
+			}
+
+			public function current_dom_get($html) {
+
+				//--------------------------------------------------
+				// Missing current URL
+
+					if ($this->current_url === NULL) {
+						exit_with_error('Need to request a page first');
+					}
+
+				//--------------------------------------------------
+				// HTML
+
+					//$html = $this->socket->response_data_get();
+					// if ($html == '') {
+					// 	return false;
+					// }
+
+					$html = file_get_contents('/Volumes/WebServer/Projects/craig.framework/framework/0.1/class/socket/data.html');
+
+				//--------------------------------------------------
+				// Parse
+
+					libxml_use_internal_errors(true);
+
+					$dom = new DOMDocument();
+					$dom->loadHTML($html);
+
+					// foreach (libxml_get_errors() as $error) {
+					// }
+					// libxml_clear_errors();
+
+				//--------------------------------------------------
+				// Return
+
+					return $dom;
+
+			}
+
+		//--------------------------------------------------
+		// Form support
+
+			public function form_select() {
+
+
+
+				//--------------------------------------------------
+				// Data
+
+
+$xpath = new DOMXPath($this->current_dom_get());
+
+
+$query = '//form[@method="POST"]';
+
+$forms = $xpath->query($query);
+
+if ($forms->length == 1) {
+	$form = $forms->item(0);
+} else {
+	exit_with_error('There were ' . $forms->length . ' forms found on: ' . $this->current_url);
+}
+
+
+debug($form);
+exit();
+
+
+			}
+
+			public function form_var_set() {
+			}
+
+			public function form_submit() {
+			}
+
+		//--------------------------------------------------
+		// DOM support
+
+
 
 	}
 
