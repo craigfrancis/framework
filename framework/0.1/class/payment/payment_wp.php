d6f7517cc92d5f34da6aade62d90bea0ed3ce840
Craig Francis <craig@craigfrancis.co.uk>
2012-05-07 12:51:21 +0000

    Add example code to login to WP admin via socket_browser

--- framework/0.1/class/payment/payment_wp.php
+++ framework/0.1/class/payment/payment_wp.php
@@ -21,6 +21,120 @@
 		// Settlements
 
 			public function settlements() {
+
+				$data = $this->_settlement_data_get();
+
+				config::set('debug.show', false);
+				mime_set('text/plain');
+
+				exit($data);
+
+			}
+
+			private function _settlement_data_get() {
+
+				//--------------------------------------------------
+				// Config
+
+					define('WORLD_PAY_REF', 3);
+					define('WORLD_PAY_TEST_MODE', 0); // Use 100 to test, and use visa card "4911830000000"
+					define('WORLD_PAY_INST_ID', 12345);
+					define('WORLD_PAY_VAT', 20);
+					define('WORLD_PAY_SIGNATURE', 'XXX');
+					define('WORLD_PAY_MERCHANT_CODE', 'XXXX');
+					define('WORLD_PAY_ADMIN_USER', 'XXX');
+					define('WORLD_PAY_ADMIN_PASS', 'XXX');
+
+					$report_range_from = date('Y-m-d 00:00:00', strtotime('-1 week'));
+					$report_range_from = date('Y-m-d 00:00:00', strtotime('-1 day'));
+					$report_range_to = date('Y-m-d 23:59:59');
+					$report_format_name = 'All'; // Login to WP, and under the "Get Statement" report, create the format under "Manage Report Column Configurations"
+
+				//--------------------------------------------------
+				// Browser object
+
+					$browser = new socket_browser();
+
+				//--------------------------------------------------
+				// Login page
+
+					if (WORLD_PAY_TEST_MODE > 0) {
+						$next_url = 'https://secure.worldpay.com/sso/public/auth/login.html?serviceIdentifier=applicationlisttest';
+					} else {
+						$next_url = 'https://secure.worldpay.com/sso/public/auth/login.html?serviceIdentifier=merchantadmin';
+					}
+
+					$browser->get($next_url);
+
+				//--------------------------------------------------
+				// Login form
+
+					$browser->form_select();
+
+					$browser->form_field_set('username', WORLD_PAY_ADMIN_USER);
+					$browser->form_field_set('password', WORLD_PAY_ADMIN_PASS);
+
+					$browser->form_submit();
+
+				//--------------------------------------------------
+				// Logout URL
+
+					$logout_url = $browser->link_get_url('//a[contains(@href,"logoff=true")]');
+
+				//--------------------------------------------------
+				// Reports
+
+					$browser->link_follow('Reports');
+
+				//--------------------------------------------------
+				// Select merchant
+
+					if (strpos($browser->url_get(), 'selectMerchantCode.html') !== false) {
+						$browser->link_follow(WORLD_PAY_MERCHANT_CODE);
+					}
+
+				//--------------------------------------------------
+				// Select report
+
+					$browser->link_follow('Get Statement');
+
+				//--------------------------------------------------
+				// Report form
+
+					$browser->form_select('//form[@id="params"]');
+
+					$report_format_key = array_search($report_format_name, $browser->form_field_select_options_get('parameter(columnConfigId)'));
+					if ($report_format_key !== false) {
+						$browser->form_field_set('parameter(columnConfigId)', $report_format_key);
+					} else {
+						exit_with_error('Cannot find report format "' . $report_format_name . '" - login to WP as "' . WORLD_PAY_ADMIN_USER . '", and under the "Get Statement" report, create the format under "Manage Report Column Configurations"');
+					}
+
+					$browser->form_field_set('parameter(merchantCode)', WORLD_PAY_MERCHANT_CODE);
+					$browser->form_field_set('parameter(fromDate)', $report_range_from);
+					$browser->form_field_set('parameter(untilDate)', $report_range_to);
+					$browser->form_field_set('parameter(journalTypeCode)', 'ALL');
+					$browser->form_field_set('parameter(lastEvent)', '1');
+					$browser->form_field_set('format', 'csv');
+					$browser->form_field_set('action', 'show');
+
+					$browser->form_submit('reportParameterOperation', 'generate');
+
+				//--------------------------------------------------
+				// Data
+
+					$data = $browser->data_get();
+
+				//--------------------------------------------------
+				// Logout
+
+					$browser->get($logout_url);
+
+				//--------------------------------------------------
+				// Return
+
+					return $data;
+
 			}
 
 	}
