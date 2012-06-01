6e4ea425c2fb7f6def1b3eacc42f635431b2ffda
Craig Francis <craig@craigfrancis.co.uk>
2012-06-01 15:41:52 +0000

    Add support for output.css_path_prefix

--- framework/0.1/class/resources.php
+++ framework/0.1/class/resources.php
@@ -156,6 +156,8 @@
 			//--------------------------------------------------
 			// Add to config
 
+				$css_prefix = config::get('output.css_path_prefix', '');
+
 				foreach ($css_types as $css_type_name => $css_type_info) {
 
 					if ($css_type_info['default'] == true || $css_name == $css_type_name) {
@@ -163,7 +165,7 @@
 
 							$media = ($css_name == $css_type_name ? $css_type_info['media_selected'] : $css_type_info['media_normal']);
 
-							resources::css_add($path, $media);
+							resources::css_add($css_prefix . $path, $media);
 
 						}
 					}
@@ -171,7 +173,7 @@
 					if ($css_type_info['alt_title'] != '' && $css_name != $css_type_name) {
 						foreach ($css_type_info['files'] as $path) {
 
-							resources::css_alternate_add($path, 'all', $css_type_info['alt_title']);
+							resources::css_alternate_add($css_prefix . $path, 'all', $css_type_info['alt_title']);
 
 						}
 					}
