3cc2c1001489402b39b57a2731b94dedbb4840d2
Craig Francis <craig@craigfrancis.co.uk>
2012-06-08 09:05:11 +0000

    Add note about paypal payments

---
+++ framework/0.1/class/payment/payment_paypal.php
@@ -0,0 +1,6 @@
+<?php
+
+// http://net.tutsplus.com/tutorials/php/how-to-process-credit-cards-with-paypal-payments-pro-using-php/
+// https://www.x.com/developers/paypal/documentation-tools/api/dodirectpayment-api-operation-nvp
+
+?>
