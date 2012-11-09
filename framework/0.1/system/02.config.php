fcb802dfb7fade15bd2b817cdf8f2482940eff97
Craig Francis <craig@craigfrancis.co.uk>
2012-11-09 16:21:15 +0000

    Exclude Webkit < 536 for CSP (needs port number) | Update debug (inc restore loading times)

--- framework/0.1/system/02.config.php
+++ framework/0.1/system/02.config.php
@@ -284,7 +284,7 @@
 	//--------------------------------------------------
 	// Debug
 
-		config::set_default('debug.level', (SERVER == 'stage' ? 4 : 0)); // 0 not running, 1 for execution time, 2 to also include application logs, 3 for framework logs, 4+ for framework debugging.
+		config::set_default('debug.level', (SERVER == 'stage' ? 3 : 0)); // 0 not running, 1 for execution time, 2 to also include application logs, 3 for framework logs, 4+ for framework debugging.
 		config::set_default('debug.show', true); // Only relevant when running.
 		config::set_default('debug.db', (config::get('debug.level') > 1));
 		config::set_default('debug.db_required_fields', array('deleted'));
