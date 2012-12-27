86e9d2b8febb57bdaaad2bf78e700a223d1deabc
Craig Francis <craig@craigfrancis.co.uk>
2012-12-27 16:10:01 +0000

    Report errors when using the 'loading' helper (don't die silently) | Allow response set() method to accept an array of variables

--- framework/0.1/class/loading.php
+++ framework/0.1/class/loading.php
@@ -218,7 +218,7 @@
 								$response->render_error('system');
 								exit();
 							} else {
-								exit('Loading error: ' . (isset($error['message']) ? $error['message'] : 'Unknown'));
+								exit('<p>Loading error: ' . html(isset($error['message']) ? $error['message'] : 'Unknown') . '</p>');
 							}
 
 						}
