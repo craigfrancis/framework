774e12c81990de01f6fa178d9d6f484f4eba0718
Craig Francis <craig@craigfrancis.co.uk>
2013-01-07 11:12:12 +0000

    Updated examples

--- app/library/examples/form/10-date-select.php
+++ app/library/examples/form/10-date-select.php
@@ -18,8 +18,6 @@
 	$field = new form_field_date($form, 'Date');
 	if ($database) $field->db_field_set('date');
 	$field->invalid_error_set('The date does not appear to be correct.');
-	$field->format_input_set(array('Y', 'M', 'D'));
-	$field->format_label_set(array('separator' => '-')); // Set to NULL to remove, or a string (but this looses the <label> tags)
 	$field->input_value_options_set('D', range(1, 31)); // Range creates a 0 based index, so use the array values only
 	$field->input_text_options_set('M', $months); // Is slower for data entry, but avoids the American date format confusion (MM/DD/YYYY)
 
