0000470a79a6ab6dc22bab376b0c27edbf8fccd3
Craig Francis <craig@craigfrancis.co.uk>
2013-01-08 09:52:03 +0000

    Add Google Tools verification code

---
+++ app/controller/home.php
@@ -0,0 +1,21 @@
+<?php
+
+	class home_controller extends controller {
+
+		public function action_index() {
+
+			//--------------------------------------------------
+			// Resources
+
+				$response = response_get();
+
+			//--------------------------------------------------
+			// Response
+
+				$response->head_add_html("\n\n\t" . '<meta name="google-site-verification" content="4xz-gkRYk_S0uK9yw8UzhPDTy1EZEMtLmWr4pnkGoVs" />');
+
+		}
+
+	}
+
+?>
