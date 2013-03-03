a4168d141e67c88bca877ad254595a31fe191cb9
Craig Francis <craig@craigfrancis.co.uk>
2013-03-03 18:42:58 +0000

    Update fields example (caps the Hours/Days label)

--- app/library/examples/form/fields.php
+++ app/library/examples/form/fields.php
@@ -2,7 +2,7 @@
 
 	$field = new form_field_fields($form, 'Estimate');
 	$field->input_add('V', array('size' => 3));
-	$field->input_add('U', array('options' => array('hours' => 'hours', 'days' => 'days')));
+	$field->input_add('U', array('options' => array('hours' => 'Hours', 'days' => 'Days')));
 	$field->required_error_set('The estimate is required.');
 	$field->invalid_error_set('An invalid estimate value has been set.');
 
