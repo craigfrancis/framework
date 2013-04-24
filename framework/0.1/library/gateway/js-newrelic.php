b00d8ab8fc0f3b9bf89bed4dc45aa8a6694df7c8
Craig Francis <craig@craigfrancis.co.uk>
2013-04-24 17:07:20 +0100

    Add support for head/foot JavaScript includes

--- framework/0.1/library/gateway/js-newrelic.php
+++ framework/0.1/library/gateway/js-newrelic.php
@@ -34,7 +34,7 @@
 
 			mime_set('application/javascript');
 
-			http_cache_headers(60*60*24*7);
+			http_cache_headers(60*60*12, time());
 
 		//--------------------------------------------------
 		// Content
