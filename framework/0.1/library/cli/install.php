b45e651098f32a668aff00dd512bc166c2ddf1b7
Craig Francis <craig@craigfrancis.co.uk>
2013-07-15 13:32:31 +0100

    Add compare_db() function

--- framework/0.1/library/cli/install.php
+++ framework/0.1/library/cli/install.php
@@ -131,24 +131,20 @@
 		//--------------------------------------------------
 		// File folders
 
-			$setup_folder = APP_ROOT . '/library/setup';
-
 			$folders = array(
-				'files' => FILE_ROOT,
-				'private' => PRIVATE_ROOT,
-			);
+					APP_ROOT . '/library/setup/dir.files.txt' => FILE_ROOT,
+					APP_ROOT . '/library/setup/dir.private.txt' => PRIVATE_ROOT,
+				);
 
-			foreach ($folders as $folder_name => $folder_path) {
+			foreach ($folders as $setup_path => $folder_path) {
 
 				if (substr($folder_path, -1) != '/') {
 					$folder_path .= '/';
 				}
 
-				$setup_file = $setup_folder . '/dir.' . safe_file_name($folder_name) . '.txt';
+				if (is_file($setup_path)) {
 
-				if (is_file($setup_file)) {
-
-					$folder_children = explode("\n", file_get_contents($setup_file));
+					$folder_children = explode("\n", file_get_contents($setup_path));
 
 					foreach ($folder_children as $path) {
 						$path = $folder_path . $path;
@@ -191,7 +187,33 @@
 		//--------------------------------------------------
 		// Check database structure
 
-			// TODO
+			$setup_path = APP_ROOT . '/library/setup/database.txt';
+
+			if (is_file($setup_path)) {
+
+				$stored_db = json_decode(file_get_contents($setup_path), true); // Assoc array
+
+				if ($stored_db !== NULL) {
+
+					$output = '';
+
+					foreach (compare_db($stored_db, dump_db()) as $table_name => $table_details) {
+						if (count($table_details) > 0) {
+							$output .= $table_name . "\n";
+							foreach ($table_details as $table_detail) {
+								$output .= '  ' . $table_detail . "\n";
+							}
+							$output .= "\n";
+						}
+					}
+
+					if ($output != '') {
+						echo "\n" . $output;
+					}
+
+				}
+
+			}
 
 		//--------------------------------------------------
 		// Run install script
@@ -200,6 +222,131 @@
 			if (is_file($install_root)) {
 				script_run($install_root);
 			}
+
+	}
+
+	function compare_db($a, $b) {
+
+		$details = array();
+
+		foreach ($a as $table => $a_table_info) {
+
+			//--------------------------------------------------
+			// Table
+
+				$details[$table] = array();
+
+				if (!isset($b[$table])) {
+					$details[$table][] = 'Table: Missing in current database.';
+					continue;
+				}
+
+			//--------------------------------------------------
+			// Fields
+
+				foreach ($a_table_info['fields'] as $a_field_name => $a_field_info) {
+
+					if (!isset($b[$table]['fields'][$a_field_name])) {
+						$details[$table][] = 'Field: Missing "' . $a_field_name . '" in current database.';
+						continue;
+					}
+
+					$b_field_info = $b[$table]['fields'][$a_field_name];
+
+					foreach ($a_field_info as $a_info_name => $a_info_value) {
+						if (!array_key_exists($a_info_name, $b_field_info)) {
+
+							$details[$table][] = 'Field: Unknown "' . $a_field_name . '.' . $a_info_name . '" propertly in current database.';
+							continue;
+
+						} else if ($b_field_info[$a_info_name] != $a_info_value && !in_array($a_info_name, array('multiple_key'))) {
+
+							$details[$table][] = 'Field: Changed "' . $a_field_name . '.' . $a_info_name . '" propertly ("' . $a_info_value . '" != "' . $b_field_info[$a_info_name] . '").';
+							continue;
+
+						}
+					}
+
+				}
+
+				$a_fields = array_keys($a_table_info['fields']);
+				$b_fields = array_keys($b[$table]['fields']);
+				if (count($a_fields) == count($b_fields)) {
+					foreach ($a_fields as $a_offset => $a_name) {
+						if ($b_fields[$a_offset] != $a_name) {
+							$details[$table][] = 'Field: Order "' . $a_offset . '" is different ("' . $a_name . '" != "' . $b_fields[$a_offset] . '").';
+							break; // No point in continuing this check
+						}
+					}
+				}
+
+				foreach ($b[$table]['fields'] as $b_field_name => $b_field_info) {
+					if (!isset($a[$table]['fields'][$b_field_name])) {
+						$details[$table][] = 'Field: Created "' . $b_field_name . '" in current database.';
+						continue;
+					}
+				}
+
+			//--------------------------------------------------
+			// Keys
+
+				foreach ($a_table_info['keys'] as $a_key_name => $a_key_info) {
+
+					if (!isset($b[$table]['keys'][$a_key_name])) {
+						$details[$table][] = 'Key: missing "' . $a_key_name . '" in current database.';
+						continue;
+					}
+
+					$b_key_info = $b[$table]['keys'][$a_key_name];
+
+					if (count($b_key_info) != count($a_key_info)) {
+
+						$details[$table][] = 'Key: Changed "' . $a_key_name . '" in current database (missing field).';
+						continue;
+
+					} else {
+						foreach ($a_key_info as $a_key_field_seq => $a_key_field_info) {
+
+							if (!isset($b[$table]['keys'][$a_key_name][$a_key_field_seq])) {
+
+								$details[$table][] = 'Key: Changed "' . $a_key_name . '" in current database (missing field ' . $a_key_field_seq . ').';
+								continue;
+
+							} else {
+
+								$b_key_field_info = $b[$table]['keys'][$a_key_name][$a_key_field_seq];
+
+								foreach ($a_key_field_info as $a_name => $a_value) {
+									if (!array_key_exists($a_name, $b_key_field_info)) {
+
+										$details[$table][] = 'Key: Unknown "' . $a_key_name . '.' . $a_key_field_seq . '.' . strtolower($a_name) . '" property in current database.';
+										continue;
+
+									} else if ($b_key_field_info[$a_name] != $a_value && !in_array($a_name, array('cardinality'))) {
+
+										$details[$table][] = 'Key: Changed "' . $a_key_name . '.' . $a_key_field_seq . '.' . strtolower($a_name) . '" property ("' . $b_key_field_info[$a_name] . '" != "' . $a_value . '").';
+										continue;
+
+									}
+								}
+
+							}
+
+						}
+					}
+
+				}
+
+				foreach ($b[$table]['keys'] as $b_key_name => $b_key_info) {
+					if (!isset($a[$table]['keys'][$b_key_name])) {
+						$details[$table][] = 'Key: Created "' . $b_key_name . '" in current database.';
+						continue;
+					}
+				}
+
+		}
+
+		return $details;
 
 	}
 
