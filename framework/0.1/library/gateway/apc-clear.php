c8f73a22e256a73168c26aedbcf49732d47bf2e3
Craig Francis <craig@craigfrancis.co.uk>
2014-01-28 13:01:34 +0000

    Add feature to clear APC cache on mvtolive

--- framework/0.1/library/gateway/apc-clear.php
+++ framework/0.1/library/gateway/apc-clear.php
@@ -8,10 +8,7 @@
 
 	} else if ($request_key != sha1(ENCRYPTION_KEY . date('Y-m-d'))) {
 
-		echo 'Invalid key';
-		echo $request_key . '#';
-		echo ENCRYPTION_KEY . '#';
-		echo sha1(ENCRYPTION_KEY . date('Y-m-d')) . '#';
+		echo 'Invalid key (' . config::get('request.domain') . ')';
 
 	} else if (!function_exists('apc_clear_cache')) {
 
