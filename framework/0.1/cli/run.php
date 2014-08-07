7e5a3e71d1dc25e8bd21a337d8508ac7d7cf1ea4
Craig Francis <craig@craigfrancis.co.uk>
2014-08-07 11:52:53 +0100

    Option to hide config values in PRIVATE_ROOT

--- framework/0.1/cli/run.php
+++ framework/0.1/cli/run.php
@@ -9,8 +9,8 @@
 	define('REQUEST_MODE', 'cli');
 
 echo '#1#' . ROOT . "\n";
-echo '#2#' . substr(ROOT, -14) . "\n";
-	if (substr(ROOT, -14) == '/upload/files/') {
+echo '#2#' . substr(ROOT, -13) . "\n";
+	if (substr(ROOT, -13) == '/upload/files') {
 		$private_root = substr(ROOT, 0, -13) . '/private'; // We won't be uploading private files, and it may contain a "private app config" ini file.
 		if (is_dir($private_root)) {
 			define('PRIVATE_ROOT', $private_root);
