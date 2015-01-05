39a1574ad0deab6945f9034cb64bfe56b695971f
Craig Francis <craig@craigfrancis.co.uk>
2015-01-05 17:34:46 +0000

    Support a config propery in the cms-html helper

--- framework/0.1/library/class/cms/cms-html.php
+++ framework/0.1/library/class/cms/cms-html.php
@@ -2,24 +2,49 @@
 
 	class cms_html_base extends check {
 
-		public function __construct($config = NULL) {
-		}
+		//--------------------------------------------------
+		// Variables
 
-		public function process_text($text) {
-			return $text;
-		}
+			protected $config = array();
 
-		public function process_inline_html($html) {
-			return $this->clean_html($html);
-		}
+		//--------------------------------------------------
+		// Setup
 
-		public function process_block_html($html) {
-			return $this->clean_html($html);
-		}
+			public function __construct($config = NULL) {
+				$this->setup($config);
+			}
 
-		protected function clean_html($html) {
-			return str_replace('<br>', '<br />', $html); // TODO: https://github.com/craigfrancis/html-filter
-		}
+			protected function setup($config) {
+
+				if (!is_array($config)) {
+					$config = array();
+				}
+
+				$this->config = $config;
+
+			}
+
+		//--------------------------------------------------
+		// Processing
+
+			public function process_text($text) {
+				return $text;
+			}
+
+			public function process_inline_html($html) {
+				return $this->clean_html($html);
+			}
+
+			public function process_block_html($html) {
+				return $this->clean_html($html);
+			}
+
+		//--------------------------------------------------
+		// Cleanup
+
+			protected function clean_html($html) {
+				return str_replace('<br>', '<br />', $html); // TODO: https://github.com/craigfrancis/html-filter
+			}
 
 	}
 
