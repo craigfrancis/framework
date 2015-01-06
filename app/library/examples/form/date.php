04beacc644ff6f5e95d652c10ac976cb7d501403
Craig Francis <craig@craigfrancis.co.uk>
2015-01-06 09:56:47 +0000

    Update note about bday autocomplete value (HTML 5.1)

--- app/library/examples/form/date.php
+++ app/library/examples/form/date.php
@@ -2,7 +2,7 @@
 
 	$field = new form_field_date($form, 'Date');
 	if ($database) $field->db_field_set('date');
-	$field->autocomplete_set('bday'); // Special value, according to WHATWG spec
+	$field->autocomplete_set('bday'); // Special value, according to HTML 5.1 spec
 	$field->invalid_error_set('Your date does not appear to be correct.');
 	$field->required_error_set('Your date is required.');
 	$field->min_date_set('Your date has to be after 2000.', strtotime('2000-01-01'));
