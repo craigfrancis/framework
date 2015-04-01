008ef6346ecbf2342fd92a47360c3256d007e46a
Craig Francis <craig@craigfrancis.co.uk>
2015-04-01 10:18:02 +0100

    Set output.js_default_defer to on by default

--- framework/0.1/includes/02.config.php
+++ framework/0.1/includes/02.config.php
@@ -268,6 +268,7 @@
 		config::set_default('output.favicon_path', ASSET_ROOT . '/img/global/favicon.ico');
 		config::set_default('output.timestamp_url', false);
 		config::set_default('output.js_combine', true);
+		config::set_default('output.js_default_defer', true);
 		config::set_default('output.js_min', false);
 		config::set_default('output.css_min', false);
 		config::set_default('output.css_name', '');
