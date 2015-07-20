9c129f80fe6af853949fab3c2804bc78cb302659
Craig Francis <craig@craigfrancis.co.uk>
2015-07-20 16:48:05 +0100

    Pass NULL though the number field (e.g. input not shown on form)

--- framework/0.1/library/class/form/form-field-number.php
+++ framework/0.1/library/class/form/form-field-number.php
@@ -139,8 +139,10 @@
 			public function value_set($value) {
 				if ($value === '') {
 					$this->value_clean = 0;
-				} else {
+				} else if ($value !== NULL) {
 					$this->value_clean = parse_number($value);
+				} else {
+					$this->value_clean = NULL;
 				}
 				$this->value = $value;
 			}
@@ -156,7 +158,12 @@
 			protected function _value_print_get($decimal_places = 2) {
 
 				$value = parent::_value_print_get(); // Value from $this->value, saved_value, or database
-				$value_clean = parse_number($value);
+
+				if ($value === NULL) {
+					$value_clean = NULL;
+				} else {
+					$value_clean = parse_number($value);
+				}
 
 				if ($value_clean !== NULL) {
 					if ($value_clean == 0 && $this->zero_to_blank && $this->type != 'currency') {
