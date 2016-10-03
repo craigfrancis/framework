d79d9b6cc420b702f94d2507a92cbfee9a02a63f
Craig Francis <craig@craigfrancis.co.uk>
2016-10-03 10:12:12 +0100

    Restore stream_set_timeout() in socket class

--- framework/0.1/library/class/socket/socket.php
+++ framework/0.1/library/class/socket/socket.php
@@ -526,7 +526,7 @@
 
 					if ($connection) {
 
-						// stream_set_timeout($connection, $this->request_timeout);
+						stream_set_timeout($connection, $this->request_timeout);
 
 						$result = @fwrite($connection, $request); // Send request
 
