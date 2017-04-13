5d8569383351d441db18023c110745b3730b9f18
Craig Francis <craig@craigfrancis.co.uk>
2017-04-13 13:18:29 +0100

    Update process to get a controller, so it sits within a function

--- framework/0.1/includes/07.controller.php
+++ framework/0.1/includes/07.controller.php
@@ -5,7 +5,8 @@
 
 	class controller_base extends check {
 
-		public $parent;
+		public $path = NULL;
+		public $parent = NULL;
 
 		public function route() {
 		}
@@ -31,285 +32,302 @@
 //--------------------------------------------------
 // Find controller
 
-	//--------------------------------------------------
-	// Route folders
+	function controller_get($path) {
 
-		$route_folders = path_to_array(config::get('route.path'));
+		//--------------------------------------------------
+		// Config
 
-		if (count($route_folders) == 0) {
-			$route_folders[] = 'home';
-		}
+			$building_path = '';
+			$building_name = '';
+			$building_stack = array();
 
-	//--------------------------------------------------
-	// Controllers
+			$controllers = array();
+			$controller_id = 0;
+			$controller_log = array();
 
-		$building_path = '';
-		$building_name = '';
-		$building_stack = array();
+			$action_controller_id = 0;
+			$action_route_stack_used = array();
+			$action_route_stack_pending = array();
+			$action_method = NULL;
 
-		$controllers = array();
-		$controller_id = 0;
-		$controller_log = array();
+		//--------------------------------------------------
+		// Route
 
-		$action_controller_id = 0;
-		$action_controller_path = '';
-		$action_controller_name = '';
-		$action_route_stack_used = array();
-		$action_route_stack_pending = array();
-		$action_method = NULL;
+			if (is_array($path)) {
+				$route_stack = $path;
+			} else {
+				$route_stack = path_to_array($path);
+			}
 
-		$route_stack = $route_folders;
+			if (count($route_stack) == 0) {
+				$route_stack[] = 'home';
+			}
 
-		while (($folder = array_shift($route_stack)) !== NULL) {
+			while (($folder = array_shift($route_stack)) !== NULL) {
 
-			//--------------------------------------------------
-			// Don't exceed 10 controllers
+				//--------------------------------------------------
+				// Don't exceed 10 controllers
 
-				if ($controller_id++ > 10) {
+					if ($controller_id++ > 10) {
 
-					$controller_log[] = 'Controller depth limit reached';
+						$controller_log[] = 'Controller depth limit reached';
 
-					break;
+						break;
 
-				}
+					}
 
-			//--------------------------------------------------
-			// Load controller
+				//--------------------------------------------------
+				// Load controller
 
-				$building_path .= '/' . $folder;
-				$building_name .= ($building_name == '' ? '' : '-') . $folder;
-				$building_stack[] = $folder;
+					$building_path .= '/' . $folder;
+					$building_name .= ($building_name == '' ? '' : '-') . $folder;
+					$building_stack[] = $folder;
 
-				$controller_path = CONTROLLER_ROOT . $building_path . '.php';
-				$controller_name = str_replace('-', '_', $building_name) . '_controller';
+					$controller_path = CONTROLLER_ROOT . $building_path . '.php';
+					$controller_name = str_replace('-', '_', $building_name) . '_controller';
 
-				if (!is_file($controller_path)) {
-					$controller_log[] = $controller_path . ' - absent';
-					continue;
-				}
+					if (!is_file($controller_path)) {
+						$controller_log[] = $controller_path . ' - absent';
+						continue;
+					}
 
-				script_run_once($controller_path);
+					script_run_once($controller_path);
 
-				if (!class_exists($controller_name)) {
-					exit_with_error('Missing object "' . $controller_name . '" in "' . $controller_path . '"');
-				}
+					if (!class_exists($controller_name)) {
+						exit_with_error('Missing object "' . $controller_name . '" in "' . $controller_path . '"');
+					}
 
-			//--------------------------------------------------
-			// Initialise
+				//--------------------------------------------------
+				// Initialise
 
-				$controllers[$controller_id] = new $controller_name();
+					$controllers[$controller_id] = new $controller_name();
+					$controllers[$controller_id]->path = $controller_path;
 
-				if ($controller_id > 1 && isset($controllers[$controller_id - 1])) {
-					$controllers[$controller_id]->parent = $controllers[$controller_id - 1];
-				}
+					if ($controller_id > 1 && isset($controllers[$controller_id - 1])) {
+						$controllers[$controller_id]->parent = $controllers[$controller_id - 1];
+					}
 
-			//--------------------------------------------------
-			// Route modification
+				//--------------------------------------------------
+				// Route modification
 
-				$results = $controllers[$controller_id]->route();
+					$routing_results = $controllers[$controller_id]->route();
 
-				if (is_array($results)) {
+					if (is_array($routing_results)) {
 
-					$controller_log[] = $controller_path . ': ' . $controller_name . '->route() - ' . html(debug_dump($results));
+						$controller_log[] = $controller_path . ': ' . $controller_name . '->route() - ' . html(debug_dump($routing_results));
 
-					foreach ($results as $result_name => $result_value) {
+						foreach ($routing_results as $result_name => $result_value) {
 
-						if ($result_name == 'route_path_reset') {
+							if ($result_name == 'route_path_reset') {
 
-							$building_path = '';
-							$building_name = '';
-							$building_stack = array();
-							$route_stack = path_to_array($result_value);
+								$building_path = '';
+								$building_name = '';
+								$building_stack = array();
+								$route_stack = path_to_array($result_value);
 
-						} else if ($result_name == 'route_path_reset_prefix') {
+							} else if ($result_name == 'route_path_reset_prefix') {
 
-							$building_path = '';
-							$building_name = '';
-							$building_stack = array();
-							$route_stack = array_merge(path_to_array($result_value), $route_stack);
+								$building_path = '';
+								$building_name = '';
+								$building_stack = array();
+								$route_stack = array_merge(path_to_array($result_value), $route_stack);
 
-						} else if ($result_name == 'route_path_extend') {
+							} else if ($result_name == 'route_path_extend') {
 
-							$route_stack = array_merge($route_stack, path_to_array($result_value));
+								$route_stack = array_merge($route_stack, path_to_array($result_value));
 
-						} else if ($result_name == 'route_path_extend_prefix') {
+							} else if ($result_name == 'route_path_extend_prefix') {
 
-							$route_stack = array_merge(path_to_array($result_value), $route_stack);
+								$route_stack = array_merge(path_to_array($result_value), $route_stack);
 
-						} else {
+							} else {
 
-							exit_with_error('Unrecognised route result "' . $result_name . '" in "' . $controller_path . '"');
+								exit_with_error('Unrecognised route result "' . $result_name . '" in "' . $controller_path . '"');
+
+							}
+
+						}
+
+					} else {
+
+						$controller_log[] = $controller_path . ': ' . $controller_name . '->route() - no change';
+
+					}
+
+				//--------------------------------------------------
+				// Find action methods
+
+					$actions = array('action_index' => $route_stack);
+
+					$next_action = str_replace('-', '_', reset($route_stack));
+					if ($next_action) {
+						$actions['action_' . $next_action] = $route_stack;
+						array_shift($actions['action_' . $next_action]);
+					}
+
+					foreach ($actions as $method => $parameters) {
+
+						$controller_log_prefix = $controller_path . ': ' . $controller_name . '->' . $method . '(' . implode(', ', $parameters) . ') - ';
+
+						$valid = true;
+
+						if (!method_exists($controllers[$controller_id], $method)) {
+							$controller_log[] = $controller_log_prefix . 'absent';
+							$valid = false;
+						}
+
+						if ($valid) {
+
+							$reflection_method = new ReflectionMethod($controller_name, $method);
+							$reflection_parameters = $reflection_method->getParameters();
+
+							foreach ($reflection_parameters as $id => $reflection_parameter) {
+								if (!$reflection_parameter->isOptional() && !isset($parameters[$id])) {
+									$controller_log[] = $controller_log_prefix . 'absent';
+									$valid = false;
+								}
+							}
+
+						}
+
+						if ($valid && count($parameters) > count($reflection_parameters)) {
+							$controller_log[] = $controller_log_prefix . 'absent';
+							$valid = false;
+						}
+
+						if ($valid) {
+
+							$action_controller_id = $controller_id;
+							$action_route_stack_used = $building_stack;
+							$action_route_stack_pending = $route_stack; // Don't use $parameters, as this non "action_index" method will move to $action_route_stack_used
+							$action_method = $method;
+
+							$controller_log[] = $controller_log_prefix . 'found';
 
 						}
 
 					}
+
+			}
+
+		//--------------------------------------------------
+		// Include based controller
+
+			if ($action_controller_id == 0) {
+
+				if ($building_path == '/home') {
+					$building_path = '';
+				}
+
+				$building_path .= '/index';
+
+				$controller_path = CONTROLLER_ROOT . $building_path . '.php';
+
+				if (!is_file($controller_path)) {
+
+					$controller_log[] = $controller_path . ' - absent';
 
 				} else {
 
-					$controller_log[] = $controller_path . ': ' . $controller_name . '->route() - no change';
+					class controller_index extends controller {
 
-				}
-
-				unset($results);
-
-			//--------------------------------------------------
-			// Find action methods
-
-				$actions = array('action_index' => $route_stack);
-
-				$next_action = str_replace('-', '_', reset($route_stack));
-				if ($next_action) {
-					$actions['action_' . $next_action] = $route_stack;
-					array_shift($actions['action_' . $next_action]);
-				}
-
-				foreach ($actions as $method => $parameters) {
-
-					$controller_log_prefix = $controller_path . ': ' . $controller_name . '->' . $method . '(' . implode(', ', $parameters) . ') - ';
-
-					$valid = true;
-
-					if (!method_exists($controllers[$controller_id], $method)) {
-						$controller_log[] = $controller_log_prefix . 'absent';
-						$valid = false;
-					}
-
-					if ($valid) {
-
-						$reflection_method = new ReflectionMethod($controller_name, $method);
-						$reflection_parameters = $reflection_method->getParameters();
-
-						foreach ($reflection_parameters as $id => $reflection_parameter) {
-							if (!$reflection_parameter->isOptional() && !isset($parameters[$id])) {
-								$controller_log[] = $controller_log_prefix . 'absent';
-								$valid = false;
-							}
+						public function action_index() {
+							require($this->path);
 						}
 
 					}
 
-					if ($valid && count($parameters) > count($reflection_parameters)) {
-						$controller_log[] = $controller_log_prefix . 'absent';
-						$valid = false;
-					}
+					$controller_id++;
 
-					if ($valid) {
+					$controllers[$controller_id] = new controller_index();
+					$controllers[$controller_id]->path = $controller_path;
 
-						$action_controller_id = $controller_id;
-						$action_controller_path = $controller_path;
-						$action_controller_name = $controller_name;
-						$action_route_stack_used = $building_stack;
-						$action_route_stack_pending = $route_stack; // Don't use $parameters, as this non "action_index" method will move to $action_route_stack_used
-						$action_method = $method;
+					$action_controller_id = $controller_id;
+					$action_route_stack_used = $building_stack;
+					$action_route_stack_pending = $route_stack;
+					$action_method = 'action_index';
 
-						$controller_log[] = $controller_log_prefix . 'found';
+					$controller_log[] = $controller_path . ' - found';
 
-					}
-
-				}
-
-				unset($actions, $next_action, $method, $parameters, $controller_log_prefix, $valid, $reflection_method, $reflection_parameters, $reflection_parameter);
-
-		}
-
-	//--------------------------------------------------
-	// Include based controller
-
-		$building_path .= '/index';
-
-		$controller_path = CONTROLLER_ROOT . $building_path . '.php';
-
-		if (!is_file($controller_path)) {
-
-			$controller_log[] = $controller_path . ' - absent';
-
-		} else {
-
-			class controller_index extends controller {
-
-				public $action_index_path;
-
-				public function action_index() {
-					require_once($this->action_index_path);
 				}
 
 			}
 
-			$controller_id++;
+		//--------------------------------------------------
+		// Debug
 
-			$controllers[$controller_id] = new controller_index();
-			$controllers[$controller_id]->action_index_path = $controller_path;
+			if (config::get('debug.level') >= 3) {
 
-			$action_controller_id = $controller_id;
-			$action_controller_path = $controller_path;
-			$action_controller_name = NULL;
-			$action_route_stack_used = $building_stack;
-			$action_route_stack_pending = $route_stack;
-			$action_method = 'action_index';
+				$note_html = '<strong>Controllers</strong>:<br />' . "\n";
 
-			$controller_log[] = $controller_path . ' - found';
+				foreach ($controller_log as $log) {
+					$log_html = html($log);
+					$log_html = str_replace(' - no change', ' - <span class="debug_unchanged">no change</span>', $log_html);
+					$log_html = str_replace(' - found', ' - <span class="debug_found">found</span>', $log_html);
+					$log_html = str_replace(' - absent', ' - <span class="debug_absent">absent</span>', $log_html);
+					$log_html = preg_replace('/^([^:]+:)([^\(\)]*(\(\))?)/', '\1 <strong>\2</strong>', $log_html);
+					$note_html .= '&#xA0; ' . $log_html . '<br />' . "\n";
+				}
 
-		}
+				debug_note_html(str_replace(ROOT, '', $note_html), 'H');
 
-	//--------------------------------------------------
-	// Debug
+				unset($note_html, $log);
 
-		if (config::get('debug.level') >= 3) {
-
-			$note_html = '<strong>Controllers</strong>:<br />' . "\n";
-
-			foreach ($controller_log as $log) {
-				$log_html = html($log);
-				$log_html = str_replace(' - no change', ' - <span class="debug_unchanged">no change</span>', $log_html);
-				$log_html = str_replace(' - found', ' - <span class="debug_found">found</span>', $log_html);
-				$log_html = str_replace(' - absent', ' - <span class="debug_absent">absent</span>', $log_html);
-				$log_html = preg_replace('/^([^:]+:)([^\(\)]*(\(\))?)/', '\1 <strong>\2</strong>', $log_html);
-				$note_html .= '&#xA0; ' . $log_html . '<br />' . "\n";
 			}
 
-			debug_note_html(str_replace(ROOT, '', $note_html), 'H');
+		//--------------------------------------------------
+		// Return
 
-			unset($note_html, $log);
+			if (isset($controllers[$action_controller_id])) {
+				$controller = $controllers[$action_controller_id];
+			} else {
+				$controller = NULL;
+			}
 
-		}
+			return array($action_route_stack_used, $controller, $action_method, $action_route_stack_pending);
 
-	//--------------------------------------------------
-	// Cleanup
+	}
 
-		unset($controller_id, $controller_path, $controller_name, $route_stack, $building_path, $building_name, $building_stack, $controller_log, $folder);
+	$route_folders = path_to_array(config::get('route.path'));
+	if (count($route_folders) == 0) {
+		$route_folders[] = 'home';
+	}
+
+	list($folders, $controller, $method, $arguments) = controller_get($route_folders);
 
 //--------------------------------------------------
 // Action
 
-	if ($action_method !== NULL) {
+	if ($method !== NULL) {
 
-		if ($action_method != 'action_index') {
-			array_push($action_route_stack_used, array_shift($action_route_stack_pending));
+		if ($method != 'action_index') {
+			array_push($folders, array_shift($arguments));
 		}
 
 		if (config::get('debug.level') >= 3) {
 
-			$note_html  = '<strong>Action</strong>: ' . html(str_replace(ROOT, '', $action_controller_path)) . '<br />' . "\n";
+			$note_html  = '<strong>Action</strong>: ' . html(str_replace(ROOT, '', $controller->path)) . '<br />' . "\n";
 
-			if ($action_controller_name !== NULL) {
-				$note_html .= '&#xA0; ' . html($action_controller_name) . '-><strong>before</strong>();<br />' . "\n";
-				$note_html .= '&#xA0; ' . html($action_controller_name) . '-><strong>' . html($action_method) . '</strong>(' . html(implode(', ', $action_route_stack_pending)) . ');<br />' . "\n";
-				$note_html .= '&#xA0; ' . html($action_controller_name) . '-><strong>after</strong>();<br />' . "\n";
+			$controller_name = get_class($controller);
+			if ($controller_name != 'controller_index') {
+				$note_html .= '&#xA0; ' . html($controller_name) . '-><strong>before</strong>();<br />' . "\n";
+				$note_html .= '&#xA0; ' . html($controller_name) . '-><strong>' . html($method) . '</strong>(' . html(implode(', ', $arguments)) . ');<br />' . "\n";
+				$note_html .= '&#xA0; ' . html($controller_name) . '-><strong>after</strong>();<br />' . "\n";
 			}
 
 			debug_note_html($note_html, 'H');
 
-			unset($note_html);
+			unset($note_html, $controller_name);
 
 		}
 
-		config::set('output.folders', $action_route_stack_used);
+		config::set('output.folders', $folders);
 
-		$controllers[$action_controller_id]->before();
+		$controller->before();
 
-		call_user_func_array(array($controllers[$action_controller_id], $action_method), $action_route_stack_pending);
+		call_user_func_array(array($controller, $method), $arguments);
 
-		$controllers[$action_controller_id]->after();
+		$controller->after();
 
 	} else {
 
@@ -360,10 +378,10 @@
 
 		debug_note_html($note_html, 'H');
 
-		unset($note_html, $id, $value);
+		unset($note_html, $id, $value, $request_folders);
 
 	}
 
-	unset($controllers, $action_method, $action_controller_id, $action_controller_name, $action_controller_path, $action_route_stack_used, $action_route_stack_pending, $route_folders);
+	unset($controller, $method, $folders, $arguments, $route_folders, $include_path);
 
 ?>
