391db16e0a10e63c76edd7418848594cb84f86b9
Craig Francis <craig@craigfrancis.co.uk>
2017-04-13 18:31:06 +0100

    Correct HTML encoding

--- framework/0.1/includes/08.process.php
+++ framework/0.1/includes/08.process.php
@@ -22,9 +22,9 @@
 
 			$controller_name = get_class($controller);
 			if ($controller_name != 'controller_index') {
-				$note_html .= '&#xA0; ' . html($controller_name) . '-><strong>before</strong>();<br />' . "\n";
-				$note_html .= '&#xA0; ' . html($controller_name) . '-><strong>' . html($method) . '</strong>(' . html(implode(', ', $arguments)) . ');<br />' . "\n";
-				$note_html .= '&#xA0; ' . html($controller_name) . '-><strong>after</strong>();<br />' . "\n";
+				$note_html .= '&#xA0; ' . html($controller_name) . '-&gt;<strong>before</strong>();<br />' . "\n";
+				$note_html .= '&#xA0; ' . html($controller_name) . '-&gt;<strong>' . html($method) . '</strong>(' . html(implode(', ', $arguments)) . ');<br />' . "\n";
+				$note_html .= '&#xA0; ' . html($controller_name) . '-&gt;<strong>after</strong>();<br />' . "\n";
 			}
 
 			debug_note_html($note_html, 'H');
@@ -56,26 +56,26 @@
 		$note_html = '<strong>Methods</strong>:<br />' . "\n";
 
 		$note_html .= '&#xA0; $response = response_get();<br />' . "\n";
-		$note_html .= '&#xA0; $response-><strong>template_set</strong>(\'default\');<br />' . "\n";
-		$note_html .= '&#xA0; $response-><strong>view_path_set</strong>(VIEW_ROOT . \'/file.ctp\');<br />' . "\n";
-		$note_html .= '&#xA0; $response-><strong>page_id_set</strong>(\'example_ref\');<br />' . "\n";
-		$note_html .= '&#xA0; $response-><strong>title_set</strong>(\'Custom page title.\');<br />' . "\n";
-		$note_html .= '&#xA0; $response-><strong>title_full_set</strong>(\'Custom page title.\');<br />' . "\n";
+		$note_html .= '&#xA0; $response-&gt;<strong>template_set</strong>(\'default\');<br />' . "\n";
+		$note_html .= '&#xA0; $response-&gt;<strong>view_path_set</strong>(VIEW_ROOT . \'/file.ctp\');<br />' . "\n";
+		$note_html .= '&#xA0; $response-&gt;<strong>page_id_set</strong>(\'example_ref\');<br />' . "\n";
+		$note_html .= '&#xA0; $response-&gt;<strong>title_set</strong>(\'Custom page title.\');<br />' . "\n";
+		$note_html .= '&#xA0; $response-&gt;<strong>title_full_set</strong>(\'Custom page title.\');<br />' . "\n";
 
 		foreach (config::get('output.title_folders') as $id => $value) {
-			$note_html .= '&#xA0; $response-><strong>title_folder_set</strong>(' . html($id) . ', \'new_value\'); <span class="comment">// ' . html($value) . '</span><br />' . "\n";
+			$note_html .= '&#xA0; $response-&gt;<strong>title_folder_set</strong>(' . html($id) . ', \'new_value\'); <span class="comment">// ' . html($value) . '</span><br />' . "\n";
 		}
 
-		$note_html .= '&#xA0; $response-><strong>description_set</strong>(\'Page description.\');<br />' . "\n";
-		$note_html .= '&#xA0; $response-><strong>csp_source_add</strong>(\'frame-src\', \'https://www.example.com\');<br />' . "\n";
-		$note_html .= '&#xA0; $response-><strong>js_add</strong>(\'/path/to/file.js\');<br />' . "\n";
-		$note_html .= '&#xA0; $response-><strong>js_code_add</strong>(\'var x = \' . json_encode($x) . \';\');<br />' . "\n";
-		$note_html .= '&#xA0; $response-><strong>css_auto</strong>();<br />' . "\n";
-		$note_html .= '&#xA0; $response-><strong>css_add</strong>(\'/path/to/file.css\');<br />' . "\n";
-		$note_html .= '&#xA0; $response-><strong>css_alternate_add</strong>(\'/path/to/file.css\', \'print\');<br />' . "\n";
-		$note_html .= '&#xA0; $response-><strong>css_alternate_add</strong>(\'/path/to/file.css\', \'all\', \'Title\');<br />' . "\n";
-		$note_html .= '&#xA0; $response-><strong>head_add_html</strong>(\'&lt;html&gt;\');<br />' . "\n";
-		$note_html .= '&#xA0; $response-><strong>mime_set</strong>(\'text/plain\');<br />' . "\n";
+		$note_html .= '&#xA0; $response-&gt;<strong>description_set</strong>(\'Page description.\');<br />' . "\n";
+		$note_html .= '&#xA0; $response-&gt;<strong>csp_source_add</strong>(\'frame-src\', \'https://www.example.com\');<br />' . "\n";
+		$note_html .= '&#xA0; $response-&gt;<strong>js_add</strong>(\'/path/to/file.js\');<br />' . "\n";
+		$note_html .= '&#xA0; $response-&gt;<strong>js_code_add</strong>(\'var x = \' . json_encode($x) . \';\');<br />' . "\n";
+		$note_html .= '&#xA0; $response-&gt;<strong>css_auto</strong>();<br />' . "\n";
+		$note_html .= '&#xA0; $response-&gt;<strong>css_add</strong>(\'/path/to/file.css\');<br />' . "\n";
+		$note_html .= '&#xA0; $response-&gt;<strong>css_alternate_add</strong>(\'/path/to/file.css\', \'print\');<br />' . "\n";
+		$note_html .= '&#xA0; $response-&gt;<strong>css_alternate_add</strong>(\'/path/to/file.css\', \'all\', \'Title\');<br />' . "\n";
+		$note_html .= '&#xA0; $response-&gt;<strong>head_add_html</strong>(\'&lt;html&gt;\');<br />' . "\n";
+		$note_html .= '&#xA0; $response-&gt;<strong>mime_set</strong>(\'text/plain\');<br />' . "\n";
 
 		$note_html .= '&#xA0; <strong>error_send</strong>(\'page-not-found\');<br />' . "\n";
 		$note_html .= '&#xA0; <strong>message_set</strong>(\'The item has been updated.\');<br />' . "\n";
