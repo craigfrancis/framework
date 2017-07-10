fb6280a783675d7371680880a370022c34ce1d2d
Craig Francis <craig@craigfrancis.co.uk>
2017-07-10 16:46:40 +0100

    Add notes about a command helper

---
+++ framework/0.1/library/class/command.php
@@ -0,0 +1,18 @@
+<?php
+
+// TODO: Not written yet.
+//
+// Allow a command to be specified, with stdin/stdout/stderr, and auto escaped arguments (to work with taint extension).
+//
+// Review:
+//   http://symfony.com/doc/current/components/process.html
+//   https://github.com/symfony/symfony/blob/master/src/Symfony/Component/Process/Process.php
+//
+// Intention to remove:
+//   command_run()
+//   passthru()
+//   shell_exec()
+//   exec()
+//   system()
+
+?>
