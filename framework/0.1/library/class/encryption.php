aae77070af4db4dec0d4f08ee4074a8430059c52
Craig Francis <craig@craigfrancis.co.uk>
2018-02-17 17:45:26 -0500

    Allow bypass of exit_with_error (e.g. handling of error) | Support associated_data in symmetric encryption

--- framework/0.1/library/class/encryption.php
+++ framework/0.1/library/class/encryption.php
@@ -1,20 +1,34 @@
 <?php
 
-	//--------------------------------------------------
+	// //--------------------------------------------------
 	//
 	// 	$key = encryption::key_symmetric_create();
 	//
 	// 	$encrypted = encryption::encode($message, $key);
 	// 	$decrypted = encryption::decode($encrypted, $key);
 	//
-	//--------------------------------------------------
+	// 	debug($decrypted);
+	//
+	// //--------------------------------------------------
+	//
+	// 	$key = encryption::key_symmetric_create();
+	// 	$associated_data = 1234; // e.g. user id
+	//
+	// 	$encrypted = encryption::encode($message, $key, $associated_data);
+	// 	$decrypted = encryption::decode($encrypted, $key, $associated_data);
+	//
+	// 	debug($decrypted);
+	//
+	// //--------------------------------------------------
 	//
 	// 	list($key_public, $key_secret) = encryption::key_asymmetric_create();
 	//
 	// 	$encrypted = encryption::encode($message, $key_public);
 	// 	$decrypted = encryption::decode($encrypted, $key_secret);
 	//
-	//--------------------------------------------------
+	// 	debug($decrypted);
+	//
+	// //--------------------------------------------------
 	//
 	// 	list($key1_public, $key1_secret) = encryption::key_asymmetric_create(); // Sender
 	// 	list($key2_public, $key2_secret) = encryption::key_asymmetric_create(); // Recipient
@@ -22,7 +36,9 @@
 	// 	$encrypted = encryption::encode($message, $key2_public, $key1_secret);
 	// 	$decrypted = encryption::decode($encrypted, $key2_secret, $key1_public);
 	//
-	//--------------------------------------------------
+	// 	debug($decrypted);
+	//
+	// //--------------------------------------------------
 	//
 	// 	if (encryption::upgradable($key)) {
 	// 		// Generate new key, and re-encrypt.
@@ -32,7 +48,15 @@
 	// 		// Re-encrypt with new key.
 	// 	}
 	//
-	//--------------------------------------------------
+	// //--------------------------------------------------
+	//
+	// config::set('encryption.version', 2);
+	// config::set('encryption.error_return', false); // or could be NULL
+	//
+	// //--------------------------------------------------
+
+	config::set_default('encryption.version', NULL);
+	config::set_default('encryption.error_return', -1);
 
 	class encryption_base extends check {
 
@@ -40,7 +64,7 @@
 
 		public static function key_symmetric_create() {
 
-			if (function_exists('sodium_crypto_aead_chacha20poly1305_ietf_encrypt')) {
+			if (function_exists('sodium_crypto_aead_chacha20poly1305_ietf_encrypt') && config::get('encryption.version') !== 1) {
 
 				return 'KS2-' . base64_encode(sodium_crypto_aead_chacha20poly1305_ietf_keygen());
 
@@ -54,7 +78,7 @@
 
 		public static function key_asymmetric_create() {
 
-			if (function_exists('sodium_crypto_box')) {
+			if (function_exists('sodium_crypto_box') && config::get('encryption.version') !== 1) {
 
 				$keypair = sodium_crypto_box_keypair();
 
@@ -132,19 +156,19 @@
 			list($key1_type, $key1_value) = array_pad(explode('-', $key1), 2, NULL);
 			list($key2_type, $key2_value) = array_pad(explode('-', $key2), 2, NULL);
 
-			if ($key1_type === 'KS2' && $key2_type === '' && $key2_value === NULL) {
+			if ($key1_type === 'KS2') {
 
 				$key = base64_decode($key1_value); // Base64 encoding is not "constant time", which might be an issue, but unlikely considering a network connection would introduce ~5ms delays ... https://twitter.com/CiPHPerCoder/status/947251405911412739 ... https://paragonie.com/blog/2016/06/constant-time-encoding-boring-cryptography-rfc-4648-and-you
 
 				$return_type = 'ES2';
-				$return_values = self::_encode_symmetric_sodium($key, $input);
+				$return_values = self::_encode_symmetric_sodium($key, $input, $key2); // key2 is associated data (e.g. user id)
 
-			} else if ($key1_type === 'KS1' && $key2_type === '' && $key2_value === NULL) {
+			} else if ($key1_type === 'KS1') {
 
 				$key = base64_decode($key1_value);
 
 				$return_type = 'ES1';
-				$return_values = self::_encode_symmetric_openssl($key, $input);
+				$return_values = self::_encode_symmetric_openssl($key, $input, $key2);
 
 			} else if ($key1_type === 'KA2P' && $key2_type === '' && $key2_value === NULL) {
 
@@ -188,20 +212,23 @@
 
 		public static function decode($input, $key1, $key2 = NULL) {
 
+			config::set('encryption.error_message', NULL);
+			config::set('encryption.error_extra', NULL);
+
 			list($input_type, $input_value, $input_nonce, $input_hmac, $input_salt) = array_pad(explode('-', $input), 5, NULL);
 
 			list($key1_type, $key1_value) = array_pad(explode('-', $key1), 2, NULL);
 			list($key2_type, $key2_value) = array_pad(explode('-', $key2), 2, NULL);
 
-			if ($input_type === 'ES2' && $key1_type === 'KS2' && $key2_type === '' && $key2_value === NULL) {
+			if ($input_type === 'ES2' && $key1_type === 'KS2') {
 
 				$key = base64_decode($key1_value);
 				$encrypted = base64_decode($input_value);
 				$nonce = base64_decode($input_nonce);
 
-				$plaintext = self::_decode_symmetric_sodium($key, $encrypted, $nonce);
+				$return = self::_decode_symmetric_sodium($key, $encrypted, $nonce, $key2);
 
-			} else if ($input_type === 'ES1' && $key1_type === 'KS1' && $key2_type === '' && $key2_value === NULL) {
+			} else if ($input_type === 'ES1' && $key1_type === 'KS1') {
 
 				$key = base64_decode($key1_value);
 				$encrypted = base64_decode($input_value);
@@ -209,14 +236,14 @@
 				$hmac = base64_decode($input_hmac);
 				$salt = base64_decode($input_salt);
 
-				$plaintext = self::_decode_symmetric_openssl($key, $encrypted, $vi, $hmac, $salt);
+				$return = self::_decode_symmetric_openssl($key, $encrypted, $vi, $hmac, $salt, $key2);
 
 			} else if ($input_type === 'EAO2' && $key1_type === 'KA2S' && $key2_type === '' && $key2_value === NULL) {
 
 				$key_secret = base64_decode($key1_value);
 				$encrypted = base64_decode($input_value);
 
-				$plaintext = self::_decode_asymmetric_one_sodium($key_secret, $encrypted);
+				$return = self::_decode_asymmetric_one_sodium($key_secret, $encrypted);
 
 			} else if ($input_type === 'EAO1' && $key1_type === 'KA1S' && $key2_type === '' && $key2_value === NULL) {
 
@@ -225,7 +252,7 @@
 				$keys_encrypted = base64_decode($input_nonce);
 				$hmac_value = base64_decode($input_hmac);
 
-				$plaintext = self::_decode_asymmetric_one_openssl($key_secret, $data_encrypted, $keys_encrypted, $hmac_value);
+				$return = self::_decode_asymmetric_one_openssl($key_secret, $data_encrypted, $keys_encrypted, $hmac_value);
 
 			} else if ($input_type === 'EAT2' && $key1_type === 'KA2S' && $key2_type === 'KA2P') {
 
@@ -234,7 +261,7 @@
 				$encrypted = base64_decode($input_value);
 				$nonce = base64_decode($input_nonce);
 
-				$plaintext = self::_decode_asymmetric_two_sodium($recipient_key_secret, $sender_key_public, $encrypted, $nonce);
+				$return = self::_decode_asymmetric_two_sodium($recipient_key_secret, $sender_key_public, $encrypted, $nonce);
 
 			} else if ($input_type === 'EAT1' && $key1_type === 'KA1S' && $key2_type === 'KA1P') {
 
@@ -244,7 +271,7 @@
 				$keys_encrypted = base64_decode($input_nonce);
 				$hmac_encrypted = base64_decode($input_hmac);
 
-				$plaintext = self::_decode_asymmetric_two_openssl($recipient_key_secret, $sender_key_public, $data_encrypted, $keys_encrypted, $hmac_encrypted);
+				$return = self::_decode_asymmetric_two_openssl($recipient_key_secret, $sender_key_public, $data_encrypted, $keys_encrypted, $hmac_encrypted);
 
 			} else {
 
@@ -252,21 +279,25 @@
 
 			}
 
-			if (is_string($plaintext)) { // i.e. did not return false.
-				return $plaintext;
+			if (is_string($return) || config::get('encryption.error_message') !== NULL) { // i.e. Either the plaintext (did not return false), or an error occurred.
+				return $return;
 			} else {
-				exit_with_error('Invalid encrypted message (' . $input_type . '/' . $key1_type . '/' . $key2_type . ')', debug_dump($plaintext));
+				return self::_error_return('Invalid encrypted message (' . $input_type . '/' . $key1_type . '/' . $key2_type . ')', debug_dump($return));
 			}
 
 		}
 
-		private static function _encode_symmetric_sodium($key, $input) { // Symmetric key ... https://paragonie.com/blog/2017/06/libsodium-quick-reference-quick-comparison-similar-functions-and-which-one-use#crypto-aead-sample-php
+		private static function _encode_symmetric_sodium($key, $input, $associated_data = NULL) { // Symmetric key ... https://paragonie.com/blog/2017/06/libsodium-quick-reference-quick-comparison-similar-functions-and-which-one-use#crypto-aead-sample-php
 
 			$nonce = random_bytes(SODIUM_CRYPTO_AEAD_CHACHA20POLY1305_IETF_NPUBBYTES);
 
+			if ($associated_data === NULL) {
+				$associated_data = $nonce;
+			}
+
 			$encrypted = sodium_crypto_aead_chacha20poly1305_ietf_encrypt(
 					$input,
-					$nonce,
+					$associated_data,
 					$nonce,
 					$key
 				);
@@ -275,18 +306,22 @@
 
 		}
 
-		private static function _decode_symmetric_sodium($key, $encrypted, $nonce) {
+		private static function _decode_symmetric_sodium($key, $encrypted, $nonce, $associated_data = NULL) {
+
+			if ($associated_data === NULL) {
+				$associated_data = $nonce;
+			}
 
 			return sodium_crypto_aead_chacha20poly1305_ietf_decrypt(
 					$encrypted,
-					$nonce,
+					$associated_data,
 					$nonce,
 					$key
 				);
 
 		}
 
-		private static function _encode_symmetric_openssl($key, $input) { // Symmetric key LEGACY ... https://paragonie.com/blog/2015/05/if-you-re-typing-word-mcrypt-into-your-code-you-re-doing-it-wrong
+		private static function _encode_symmetric_openssl($key, $input, $associated_data = NULL) { // Symmetric key LEGACY ... https://paragonie.com/blog/2015/05/if-you-re-typing-word-mcrypt-into-your-code-you-re-doing-it-wrong
 
 			$iv_size = openssl_cipher_iv_length(self::$openssl_cipher);
 			$iv = openssl_random_pseudo_bytes($iv_size);
@@ -297,20 +332,20 @@
 
 			$encrypted = openssl_encrypt($input, self::$openssl_cipher, $key_encrypt, OPENSSL_RAW_DATA, $iv);
 
-			$hmac = hash_hmac('sha256', $salt . $iv . $encrypted, $key_authenticate, true);
+			$hmac = hash_hmac('sha256', $salt . $iv . strval($associated_data) . $encrypted, $key_authenticate, true);
 
 			return [$encrypted, $iv, $hmac, $salt];
 
 		}
 
-		private static function _decode_symmetric_openssl($key, $encrypted, $iv, $hmac, $salt) {
+		private static function _decode_symmetric_openssl($key, $encrypted, $iv, $hmac, $salt, $associated_data = NULL) {
 
 			list($key_encrypt, $key_authenticate) = self::_openssl_hkdf_keys($key, $salt);
 
-			$check_hmac = hash_hmac('sha256', $salt . $iv . $encrypted, $key_authenticate, true);
+			$check_hmac = hash_hmac('sha256', $salt . $iv . strval($associated_data) . $encrypted, $key_authenticate, true);
 
 			if (!hash_equals($check_hmac, $hmac)) {
-				exit_with_error('Could not verify HMAC of the encrypted data');
+				return self::_error_return('Could not verify HMAC of the encrypted data');
 			}
 
 			return openssl_decrypt($encrypted, self::$openssl_cipher, $key_encrypt, OPENSSL_RAW_DATA, $iv);
@@ -366,7 +401,7 @@
 			$data_keys = '';
 			$result = openssl_private_decrypt($keys_encrypted, $data_keys, $key_secret, OPENSSL_PKCS1_OAEP_PADDING);
 			if ($result !== true) {
-				exit_with_error('Could not decrypt the AES keys with the secret key', openssl_error_string());
+				return self::_error_return('Could not decrypt the AES keys with the secret key', openssl_error_string());
 			} else {
 				list($data_key, $hmac_key, $iv) = array_pad(explode('-', $data_keys), 3, NULL);
 				$data_key = base64_decode($data_key);
@@ -377,7 +412,7 @@
 			$check_hmac = hash_hmac('sha256', $iv . $data_encrypted, $hmac_key, true);
 
 			if (!hash_equals($check_hmac, $hmac_value)) {
-				exit_with_error('Could not verify HMAC of the encrypted data');
+				return self::_error_return('Could not verify HMAC of the encrypted data');
 			}
 
 			return openssl_decrypt($data_encrypted, self::$openssl_cipher, $data_key, OPENSSL_RAW_DATA, $iv);
@@ -442,7 +477,7 @@
 			$data_info = '';
 			$result = openssl_public_decrypt($hmac_encrypted, $data_info, $sender_key_public, OPENSSL_PKCS1_PADDING);
 			if ($result !== true) {
-				exit_with_error('Could not decrypt the HMAC with the senders public key', openssl_error_string());
+				return self::_error_return('Could not decrypt the HMAC with the senders public key', openssl_error_string());
 			} else {
 				list($hmac_value, $iv) = array_pad(explode('-', $data_info), 2, NULL);
 				$hmac_value = base64_decode($hmac_value);
@@ -452,7 +487,7 @@
 			$data_info = '';
 			$result = openssl_private_decrypt($keys_encrypted, $data_info, $recipient_key_secret, OPENSSL_PKCS1_OAEP_PADDING);
 			if ($result !== true) {
-				exit_with_error('Could not decrypt the AES keys with the recipients secret key', openssl_error_string());
+				return self::_error_return('Could not decrypt the AES keys with the recipients secret key', openssl_error_string());
 			} else {
 				list($data_key, $hmac_key) = array_pad(explode('-', $data_info), 2, NULL);
 				$data_key = base64_decode($data_key);
@@ -462,7 +497,7 @@
 			$check_hmac = hash_hmac('sha256', $iv . $data_encrypted, $hmac_key, true);
 
 			if (!hash_equals($check_hmac, $hmac_value)) {
-				exit_with_error('Could not verify HMAC of the encrypted data');
+				return self::_error_return('Could not verify HMAC of the encrypted data');
 			}
 
 			return openssl_decrypt($data_encrypted, self::$openssl_cipher, $data_key, OPENSSL_RAW_DATA, $iv);
@@ -478,6 +513,21 @@
 					hash_hkdf('sha256', $key, $key_length, 'Authentication', $salt),
 				];
 
+		}
+
+		private static function _error_return($error, $extra_info = NULL) {
+			$error_return = config::get('encryption.error_return');
+			if ($error_return !== -1) {
+				config::set('encryption.error_message', $error);
+				config::set('encryption.error_extra', $extra_info);
+				return $error_return;
+			} else {
+				exit_with_error($error, $extra_info);
+			}
+		}
+
+		public static function error_get() {
+			return [config::get('encryption.error_message'), config::get('encryption.error_extra')];
 		}
 
 	}
