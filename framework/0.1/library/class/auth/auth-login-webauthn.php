b2785621e4b815cc7f6e95cb9e24fdfebbe18805
Craig Francis <craig@craigfrancis.co.uk>
2018-04-11 15:24:06 +0100

    Add note about WebAuthn

---
+++ framework/0.1/library/class/auth/auth-login-webauthn.php
@@ -0,0 +1,28 @@
+<?php
+
+// https://www.w3.org/TR/webauthn/
+
+	class auth_login_webauthn_base extends check {
+
+		//--------------------------------------------------
+		// Variables
+
+			protected $auth = NULL;
+
+			public function __construct($auth) {
+				$this->auth = $auth;
+			}
+
+		//--------------------------------------------------
+		// Fields
+
+
+
+		//--------------------------------------------------
+		// Actions
+
+
+
+	}
+
+?>
