90fc746573473d296e7ff86cb4491a105ef0d6f0
Craig Francis <craig@craigfrancis.co.uk>
2018-05-25 15:40:23 +0100

    Add pcntl_exec() to the command helper notes

--- framework/0.1/library/class/command.php
+++ framework/0.1/library/class/command.php
@@ -2,7 +2,19 @@
 
 // TODO: Not written yet.
 //
-// Allow a command to be specified, with stdin/stdout/stderr, and auto escaped arguments (to work with taint extension).
+// Allow a command to be specified, with stdin/stdout/stderr, and return value
+//
+// Consider a parameterised option, with auto escaped arguments (to work with taint extension):
+//
+//   command::exec('/path/to/cmd --argument ?')
+//
+//   $command = new command();
+//   $command->exec('/path/to/cmd', ['a', 'b', 'c']);
+//   echo $command->stdout_get();
+//
+// Or, maybe an array of arguments, like pcntl_exec():
+//
+//   command::exec('/path/to/cmd'. ['--argument "unsafe"']);
 //
 // Review:
 //   http://symfony.com/doc/current/components/process.html
@@ -14,5 +26,7 @@
 //   shell_exec()
 //   exec()
 //   system()
+//   popen()
+//   proc_open() ... proc_get_status(), proc_nice(), proc_terminate()
 
 ?>
