c187bbae1112a245c9b983b1b847b000e04f77de
Craig Francis <craig@craigfrancis.co.uk>
2018-07-01 17:41:06 +0100

    Add support for 'passwordrules' attribute on password fields

--- framework/0.1/library/class/form/form-field-password.php
+++ framework/0.1/library/class/form/form-field-password.php
@@ -3,6 +3,11 @@
 	class form_field_password_base extends form_field_text {
 
 		//--------------------------------------------------
+		// Variables
+
+			protected $passwordrules = NULL;
+
+		//--------------------------------------------------
 		// Setup
 
 			public function __construct($form, $label, $name = NULL) {
@@ -16,6 +21,31 @@
 				// Additional field configuration
 
 					$this->input_type = 'password';
+
+			}
+
+			public function passwordrules_set($passwordrules) {
+
+				$this->passwordrules = $passwordrules;
+
+					// https://github.com/whatwg/html/issues/3518
+					// https://developer.apple.com/password-rules/
+					// https://github.com/mozilla/standards-positions/issues/61
+
+			}
+
+		//--------------------------------------------------
+		// Attributes
+
+			protected function _input_attributes() {
+
+				$attributes = parent::_input_attributes();
+
+				if ($this->passwordrules !== NULL) {
+					$attributes['passwordrules'] = $this->passwordrules;
+				}
+
+				return $attributes;
 
 			}
 
