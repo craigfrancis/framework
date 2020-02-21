afbb8283b73ee943f51657e1dc567e236a510d4b
Craig Francis <craig@craigfrancis.co.uk>
2020-02-21 11:09:23 +0000

    Enable 'require-trusted-types-for' when 'trusted-types' is set

--- framework/0.1/includes/01.function.php
+++ framework/0.1/includes/01.function.php
@@ -1721,9 +1721,9 @@
 			$csp['require-sri-for'] = implode(' ', $config['integrity']);
 		}
 
-		// if (isset($csp['trusted-types'])) {
-		// 	$csp['require-trusted-types-for'] = "'script'"; // TODO: Enable when supported
-		// }
+		if (isset($csp['trusted-types'])) {
+			$csp['require-trusted-types-for'] = "'script'";
+		}
 
 		if (($config['report'] || !$config['enforced']) && !array_key_exists('report-uri', $csp)) { // isset returns false for NULL
 			if ($config['report'] === true) {
