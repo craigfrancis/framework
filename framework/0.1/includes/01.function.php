8d112cbe3c9f32245fc92608e3a615380512a00f
Craig Francis <craig@craigfrancis.co.uk>
2020-06-15 12:51:19 +0100

    Reduce the length of the browser_tracker cookie

--- framework/0.1/includes/01.function.php
+++ framework/0.1/includes/01.function.php
@@ -153,7 +153,7 @@
 				$browser_tracker = random_key(40);
 			}
 
-			cookie::set('b', $browser_tracker, ['expires' => '+6 months', 'same_site' => 'Lax', 'update' => true]); // Always re-send with a long expiry (so it does not expire or expose how long session_history is).
+			cookie::set('b', $browser_tracker, ['expires' => '+3 hours', 'same_site' => 'Lax', 'update' => true]); // Always re-send, not linked to session_history
 
 			config::set('request.browser_tracker', $browser_tracker);
 
