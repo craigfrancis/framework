aa386f7fba120ef0806ce972755814b219cedc50
Craig Francis <craig@craigfrancis.co.uk>
2021-01-24 17:35:27 +0000

    Debug api keys

--- framework/0.1/library/gateway/framework-db-diff.php
+++ framework/0.1/library/gateway/framework-db-diff.php
@@ -10,7 +10,8 @@
 	$auth_hash = trim(is_file($auth_path) ? file_get_contents($auth_path) : '');
 
 	if ($auth_hash != '' || quick_hash_verify($auth_provided, $auth_hash) !== true) {
-		exit('Invalid Auth');
+		report_add(debug_dump($auth_path) . "\n" . debug_dump($auth_hash) . "\n" . debug_dump($auth_provided), 'error');
+		exit('Invalid Auth' . "\n");
 	}
 
 //--------------------------------------------------
