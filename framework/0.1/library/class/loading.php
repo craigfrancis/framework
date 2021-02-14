64d0e04881be892d2e3e9dcfa4b1b873feea9b10
Craig Francis <craig@craigfrancis.co.uk>
2021-02-14 19:17:55 +0000

    Add a basic viewport meta tag in default loading HTML

--- framework/0.1/library/class/loading.php
+++ framework/0.1/library/class/loading.php
@@ -420,6 +420,7 @@
 								<head>
 									<meta charset="' . html(config::get('output.charset')) . '" />
 									<title>Loading</title>
+									<meta name="viewport" content="width=device-width, initial-scale=1" />
 									' . $refresh_html . '
 									' . $css_html . '
 								</head>
