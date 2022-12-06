9dc372885daf5222071f8f83e121e1aa3fca7e8a
Craig Francis <craig@craigfrancis.co.uk>
2022-12-06 18:23:11 +0000

    Use nl2br in timestamp html formatting

--- framework/0.1/library/class/timestamp.php
+++ framework/0.1/library/class/timestamp.php
@@ -68,7 +68,7 @@
 					if (($pos = strpos($attributes['datetime'], 'T00:00:00')) !== false) {
 						$attributes['datetime'] = substr($attributes['datetime'], 0, $pos);
 					}
-					$html = html_tag('time', $attributes) . html($this->format($format_text)) . '</time>';
+					$html = html_tag('time', $attributes) . nl2br(html($this->format($format_text))) . '</time>';
 					return new html_safe_value($html); // Not exactly safe, as the attributes are not checked.
 				}
 			}
