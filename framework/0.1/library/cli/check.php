0df5bd7c02b567dd5045781ee8412465353ed246
Craig Francis <craig@craigfrancis.co.uk>
2023-09-18 11:16:37 +0100

    Change default database from utf8mb4_unicode_ci to utf8mb4_uca1400_ai_ci

--- framework/0.1/library/cli/check.php
+++ framework/0.1/library/cli/check.php
@@ -43,7 +43,7 @@
 
 			$default_setup = array(
 					'engine' => config::get('db.engine', 'MyISAM'), // or InnoDB... use MyISAM for FULLTEXT search before MySQL 5.6.4, faster COUNT(*) on the whole table (e.g. no WHERE), 'INSERT DELAYED' before MySQL 5.7 (now not supported), ability to 'ALTER TABLE table AUTO_INCREMENT=1'
-					'collation' => config::get('db.collation', 'utf8mb4_unicode_ci'), // Avoid general, is faster, but more error prone.
+					'collation' => config::get('db.collation', 'utf8mb4_uca1400_ai_ci'), // Avoid general, is faster, but more error prone.
 					'fields' => [],
 				);
 
