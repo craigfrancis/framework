9da0f17361775685a868905b345f4a4677c34a23
Craig Francis <craig@craigfrancis.co.uk>
2024-04-18 00:31:14 +0100

    Code clean

--- framework/0.1/includes/02.config.php
+++ framework/0.1/includes/02.config.php
@@ -447,7 +447,7 @@
 		config::set_default('output.domain', config::get('request.domain')); // Can be set for CLI support in app config file
 		config::set_default('output.lang', 'en-GB');
 		config::set_default('output.timezone', date_default_timezone_get());
-		config::set_default('output.mime', (SERVER == 'stage2' ? 'application/xhtml+xml' : 'text/html'));
+		config::set_default('output.mime', (SERVER == 'stage' ? 'application/xhtml+xml' : 'text/html'));
 		config::set_default('output.charset', 'UTF-8');
 		config::set_default('output.canonical', 'auto');
 		config::set_default('output.no_cache', false);
