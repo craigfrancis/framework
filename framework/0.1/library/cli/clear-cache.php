f4fc918c08d498054e78358b9fdc512d57f927cd
Craig Francis <craig@craigfrancis.co.uk>
2026-02-03 13:09:37 +0000

    Use config to clear (OP or APC) cache, disabled by default now

--- framework/0.1/library/cli/clear-cache.php
+++ framework/0.1/library/cli/clear-cache.php
@@ -2,7 +2,12 @@
 
 	function clear_cache_run() {
 
-		if (function_exists('opcache_reset') || function_exists('apc_clear_cache')) {
+		$clear_cache = config::get('upload.' . SERVER . '.clear_cache', NULL);
+		if ($clear_cache === NULL) {
+			$clear_cache = config::get('upload.clear_cache', NULL);
+		}
+
+		if (($clear_cache) && (function_exists('opcache_reset') || function_exists('apc_clear_cache'))) {
 
 			list($gateway_url, $response) = gateway::framework_api_auth_call('framework-opcache-clear');
 
